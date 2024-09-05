55a673ce5653e12d1c4b13c13b34997e8e456c26
MathiasTensor <tensor1995@gmail.com>
2024-09-05 14:33:41 +0200

    Update unfolding.jl
    Short descript before docstring

--- src/spectra/unfolding.jl
+++ src/spectra/unfolding.jl
@@ -4,7 +4,7 @@ corner_correction(corner_angles) =  sum([(pi^2 - c^2)/(24*pi*c) for c in corner_
 weyl_law(k,A,L) =  @. (A * k^2 - L * k)/(4*pi)
 weyl_law(k,A,L,corner_angles) =  weyl_law(k,A,L) .+ corner_correction(corner_angles)
 
-
+# Constructs k from unfolded state = number to its k value using the first term from Weyl's law
 function k_at_state(state, A, L)
     a = A
     b = -L
@@ -13,6 +13,7 @@ function k_at_state(state, A, L)
     return (-b+dis)/(2*a)
 end
 
+# Constructs k from unfolded state = number to its k value using the first term from Weyl's law and taking into account the corner corrections
 function k_at_state(state, A, L, corner_angles)
     a = A
     b = -L
