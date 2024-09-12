326a39a200c88a94ab27da20e0a2f9023fa33bdd
MathiasTensor <tensor1995@gmail.com>
2024-09-12 09:35:47 +0200

    Update analysisutils.jl
    Added saving logic for the ks and tensions

--- src/utils/analysisutils.jl
+++ src/utils/analysisutils.jl
@@ -6,6 +6,22 @@ using Threads
 using Distributions
 using StatsBase
 
+
+
+
+
+
+
+
+################ SPECTRAL STATISTICS #########################
+
+
+
+
+
+
+
+
 function number_variance(E::Vector{T}, L::T) where {T<:Real}
     # By Tomaž Prosen
     Ave1 = 0.0
@@ -150,6 +166,98 @@ function unfold_with_weyl(arr::Vector{T}, A::T, L::T, C::T) where {T<:Real}
     res_diff = [abs(res[i+1] - res[i]) for i in 1:(length(res)-1)]
     return res, mean(res_diff) # Also the mean level spacing for good unfolding
 end
+
+
+
+
+
+
+
+
+
+######################### SAVING RESULTS TOOLS #################
+
+
+
+
+
+
+
+
+
+
+
+
+function save_numerical_ks_and_tensions!(ks::Vector{T}, tens::Vector{T}, filename::String) where {T<:Real}
+    df = DataFrame(k=ks, tension=tens)
+    CSV.write(filename, df)
+end
+
+function read_numerical_ks_and_tensions(filename::String)
+    df = CSV.read(filename, DataFrame)
+    ks = df.k
+    tensions = df.tension
+    return ks, tensions
+end
+
+function compute_and_save_closest_pairs!(ksA::Vector{T}, ksB::Vector{T}, A::T, filename::String; unique=true, tolerance::Float64 = 1e-6) where {T<:Real}
+    function filter_unique_by_tolerance(v::Vector{T})
+        sorted_v = sort(v)
+        filtered = [sorted_v[1]]
+        for i in eachindex(sorted_v)[2:end]
+            if abs(sorted_v[i] - filtered[end]) > tolerance
+                push!(filtered, sorted_v[i])
+            end
+        end
+        return filtered
+    end
+
+    if unique
+        ksA = filter_unique_by_tolerance(ksA)
+    end
+
+    results = []
+    for kA in ksA
+        # Find the closest element in ksB
+        kB = ksB[argmin(abs.(ksB .- kA))]  # Find kB with the smallest absolute difference
+        # Compute absolute and relative differences
+        abs_diff = abs(kA - kB)
+        relative_diff = abs_diff / abs(kA)
+        diff_A_term = (kA^2 - kB^2) / (1/(A / (4 * π)))
+        is_below_threshold = abs(diff_A_term) < 0.01
+        push!(results, (kA, kB, relative_diff, abs_diff, diff_A_term, is_below_threshold))
+    end
+    df = DataFrame(kA=[r[1] for r in results], kB=[r[2] for r in results],
+                   relative_diff=[r[3] for r in results], abs_diff=[r[4] for r in results],
+                   diff_A_term=[r[5] for r in results], below_threshold=[r[6] for r in results])
+    CSV.write(filename * ".csv", df)
+    println("Closest pairs and analysis written to $filename")
+
+    header = ["k_num", "k_anal", "Relative Diff", "Absolute Diff", "< 0.01", "Below Thresh (1/0)?"]
+    data_matrix = hcat(df.kA, df.kB, df.relative_diff, df.abs_diff, df.diff_A_term, df.below_threshold)
+    
+    # Save as a LaTeX file using PrettyTables
+    open(filename * ".tex", "w") do f
+        pretty_table(f, data_matrix, header=header, backend=Val(:latex))
+    end
+end
+
+
+
+
+
+
+
+######################### PLOTTING STATISTICS #################
+
+
+
+
+
+
+
+
+
 
 function plot_number_variance(unfolded_energies::Vector{T}, L_min::T, L_max::T; N::Int=100) where {T<:Real}
     # Theoretical number variance functions
