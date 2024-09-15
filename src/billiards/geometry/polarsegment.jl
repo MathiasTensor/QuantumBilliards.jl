4c6436d6b11e2ae1494a68c5086eb8ee82a8cc2a
MathiasTensor <tensor1995@gmail.com>
2024-09-15 19:42:36 +0200

    Update polarsegment.jl
    Hack fix for Dual types

--- src/billiards/geometry/polarsegment.jl
+++ src/billiards/geometry/polarsegment.jl
@@ -34,12 +34,12 @@ function VirtualPolarSegment(r_func::Function; origin=SVector(0.0, 0.0), rot_ang
 end
 
 # Curve function
-function curve(polar::L, t::T) where {T, L<:PolarSegments{T}}
+function curve(polar::L, t) where {T, L<:PolarSegments{T}}
     affine_map = polar.cs.affine_map
     return affine_map(polar.r_func(t))
 end
 
-function curve(polar::L, ts::AbstractArray{T,1}) where {T, L<:PolarSegments{T}}
+function curve(polar::L, ts::AbstractArray) where {T, L<:PolarSegments{T}}
     affine_map = polar.cs.affine_map
     return collect(affine_map(polar.r_func(t)) for t in ts)
 end
@@ -51,25 +51,25 @@ function tangent(polar::L, t) where {T, L<:PolarSegments{T}}
     return ForwardDiff.derivative(l -> affine_map(r_func(l)), t)
 end
 
-function tangent(polar::L, ts::AbstractArray{T,1}) where {T, L<:PolarSegments{T}}
+function tangent(polar::L, ts::AbstractArray) where {T, L<:PolarSegments{T}}
     return collect(tangent(polar, t) for t in ts)
 end
 
 
 # Arc length calculation with handling for ForwardDiff.Dual types
-function arc_length(polar::L, t::T) where {T<:Real,L<:PolarSegments{T}}
+function arc_length(polar::L, t) where {T<:Real,L<:PolarSegments{T}}
     r_prime(l) = tangent(polar, l)
     integrand(l) = sqrt(r_prime(l)[1]^2 + r_prime(l)[2]^2)
     length, _ = quadgk(integrand, 0.0, t)
     return length
 end
 
-function arc_length(polar::L, ts::AbstractArray{T,1}) where {T<:Real,L<:PolarSegments{T}}
+function arc_length(polar::L, ts::AbstractArray) where {T<:Real,L<:PolarSegments{T}}
     return collect(arc_length(polar, t) for t in ts)
 end
 
 # Helper function for arc length during construction
-function compute_arc_length_constructor(r_func::Function, affine_map::AffineMap, t::T) where {T<:Real}
+function compute_arc_length_constructor(r_func::Function, affine_map::AffineMap, t) where {T<:Real}
     r_prime_x(l) = ForwardDiff.derivative(k -> affine_map(r_func(k))[1], l)
     r_prime_y(l) = ForwardDiff.derivative(k -> affine_map(r_func(k))[2], l)    
     integrand(l) = sqrt(r_prime_x(l)^2 + r_prime_y(l)^2)
