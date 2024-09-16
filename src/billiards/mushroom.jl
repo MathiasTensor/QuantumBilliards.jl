212a52ec2a2369cbbef8507d034e73465ef08537
MathiasTensor <tensor1995@gmail.com>
2024-09-16 11:57:53 +0200

    Update mushroom.jl
    Bugfix

--- src/billiards/mushroom.jl
+++ src/billiards/mushroom.jl
@@ -123,7 +123,7 @@ end
 function make_mushroom_and_basis(stem_width::T, stem_height::T, cap_radius::T; x0=zero(T), y0=zero(T), rot_angle=zero(T)) :: Tuple{Mushroom, CornerAdaptedFourierBessel} where {T<:Real}
     mushroom = Mushroom(stem_width, stem_height, cap_radius; x0=x0, y0=y0, rot_angle=rot_angle)
     symmetry = Vector{Any}([XReflection(-1)])
-    basis = CornerAdaptedFourierBessel(10, 2*pi/3, SVector(zero(T), zero(T)), Float64(-0/2), symmetry)
+    basis = CornerAdaptedFourierBessel(10, 3*pi/2, SVector(zero(T), zero(T)), Float64(pi), symmetry; rotation_angle_discontinuity=Float64(3*pi/4))
     return mushroom, basis
 end
 
