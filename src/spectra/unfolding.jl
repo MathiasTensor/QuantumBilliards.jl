99ad8fa862cf2bc0955dd9c8e472a5913b818e76
MathiasTensor <tensor1995@gmail.com>
2024-09-16 09:57:16 +0200

    Update unfolding.jl
    Added curvature correction

--- src/spectra/unfolding.jl
+++ src/spectra/unfolding.jl
@@ -1,6 +1,21 @@
-
+using QuadGK
 
 corner_correction(corner_angles) =  sum([(pi^2 - c^2)/(24*pi*c) for c in corner_angles])
+
+function curvature_correction(billiard::Bi) where {Bi<:AbsBilliard}
+    let segments = billiard.full_boundary
+        curvat = 0.0
+        for seg in segments 
+            if typeof(seg) == PolarSegment
+                curvat += quadgk(t -> curvature(seg, t), 0.0, 1.0)
+            end
+            if typeof(seg) == CircleSegment
+                curvat += 1/(12*pi)*(1/seg.radius)*seg.length
+            end
+        end
+        return curvat
+    end
+end
 
 weyl_law(k,A,L) =  @. (A * k^2 - L * k)/(4*pi)
 weyl_law(k,A,L,corner_angles) =  weyl_law(k,A,L) .+ corner_correction(corner_angles)
