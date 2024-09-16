d583ebf48e4fde6716dea6eca02116e3c69a335d
MathiasTensor <tensor1995@gmail.com>
2024-09-16 12:55:30 +0200

    Shift reflection
    Solving the shited reflection problem

--- src/states/wavefunctions.jl
+++ src/states/wavefunctions.jl
@@ -201,7 +201,15 @@ function wavefunction(state::S; b=5.0, inside_only=true, fundamental_domain = tr
         Psi2d::Array{type,2} = reshape(Psi, (nx,ny))
         if ~fundamental_domain 
             if ~isnothing(symmetries)
-                Psi2d, x_grid, y_grid = reflect_wavefunction(Psi2d,x_grid,y_grid,symmetries)
+                x_axis = 0.0
+                y_axis = 0.0
+                if hasproperty(billiard, :x_axis)
+                    x_axis = billiard.x_axis
+                end
+                if hasproperty(billiard, :y_axis)
+                    y_axis = billiard.y_axis
+                end
+                Psi2d, x_grid, y_grid = reflect_wavefunction(Psi2d,x_grid,y_grid,symmetries; x_axis=x_axis, y_axis=y_axis)
             end
         end
         return Psi2d, x_grid, y_grid
--- src/utils/symmetry.jl
+++ src/utils/symmetry.jl
@@ -24,17 +24,17 @@ function XYReflection(parity_x, parity_y)
     return Reflection(reflect_x ∘ reflect_y, [parity_x, parity_y], :origin)
 end
 
-function reflect_wavefunction(Psi,x_grid,y_grid,symmetries)
+function reflect_wavefunction(Psi,x_grid,y_grid,symmetries; x_axis=0.0, y_axis=0.0)
     for sym in symmetries
         if sym.axis == :y_axis
-            x = -reverse(x_grid)
+            x = 2*x_axis .- reverse(x_grid)
             Psi_ref = reverse(sym.parity.*Psi; dims=1)
 
             Psi = vcat(Psi_ref,Psi)
             x_grid = append!(x,x_grid)
         end
         if sym.axis == :x_axis
-            y = -reverse(y_grid)
+            y = 2*y_axis .- reverse(y_grid)
             Psi_ref = reverse(sym.parity.*Psi; dims=2)
 
             Psi = hcat(Psi_ref,Psi)
@@ -43,13 +43,13 @@ function reflect_wavefunction(Psi,x_grid,y_grid,symmetries)
         if sym.axis == :origin
             # Reflect over both axes (x -> -x, y -> -y)
             # First, reflect over y-axis
-            x_reflected = -reverse(x_grid)
+            x_reflected = 2*x_axis .- reverse(x_grid)
             Psi_y_reflected = reverse(sym.parity[1] .* Psi; dims=2)
             Psi_y_combined = [Psi_y_reflected Psi]
             x_grid_combined = [x_reflected; x_grid]
             
             # Then, reflect over x-axis
-            y_reflected = -reverse(y_grid)
+            y_reflected = 2*y_axis .- reverse(y_grid)
             Psi_x_reflected = reverse(sym.parity[2] .* Psi_y_combined; dims=1)
             Psi_x_combined = [Psi_x_reflected; Psi_y_combined]
             y_grid_combined = [y_reflected; y_grid]
