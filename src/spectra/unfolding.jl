6a4c9bbe705353610f949a8df7e5276dbb979d36
MathiasTensor <tensor1995@gmail.com>
2024-09-18 11:51:54 +0200

    Update unfolding.jl

--- src/spectra/unfolding.jl
+++ src/spectra/unfolding.jl
@@ -38,7 +38,7 @@ function curvature_correction(billiard::Bi; fundamental::Bool=true) where {Bi<:A
             if seg isa PolarSegment
                 println("polar segment: ", nameof(typeof(seg)), " , L: ", seg.length)
                 println("First point: ", seg.r_func(0.0))
-                println("First point: ", seg.r_func(1.0))
+                println("Last point: ", seg.r_func(1.0))
                 curvat += 1/(12*pi)*quadgk(t -> curvature(seg, t), 0.0, 1.0)[1]
             end
             if seg isa CircleSegment
