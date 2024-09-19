154cf0ec9e2474cdb8d9900b2e589ff6fd84c517
MathiasTensor <tensor1995@gmail.com>
2024-09-19 16:24:39 +0200

    Update spectralutils.jl

--- src/spectra/spectralutils.jl
+++ src/spectra/spectralutils.jl
@@ -116,6 +116,9 @@ function compute_spectrum(solver::AbsSolver, basis::AbsBasis, billiard::AbsBilli
     dk_values = []
     while k0 < k2
         dk = N_expect / (billiard.area_fundamental * k0 / (2*pi) - billiard.length_fundamental/(4*pi))
+        if dk < 0.0
+            dk = -dk
+        end
         if dk > dk_threshold # For small k this limits the size of the interval
             dk = dk_threshold
         end
