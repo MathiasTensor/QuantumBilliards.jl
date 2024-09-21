a9456c518463dc6f41e6661be7cad27e6ddd4c9a
MathiasTensor <tensor1995@gmail.com>
2024-09-21 21:05:02 +0200

    Update QuantumBilliards.jl

--- src/QuantumBilliards.jl
+++ src/QuantumBilliards.jl
@@ -91,7 +91,7 @@ include("spectra/spectralutils.jl")
 include("spectra/unfolding.jl")
 export weyl_law
 export compute_spectrum
-export curvature_correction, corner_correction
+export curvature_correction, corner_correction, curvature_and_corner_corrections
 #states
 #include("states/States.jl")
 #@reexport using .States
