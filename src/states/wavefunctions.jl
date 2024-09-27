184256d240cdb7054af669a30afc5e3aba32562f
MathiasTensor <tensor1995@gmail.com>
2024-09-27 12:31:06 +0200

    Update wavefunctions.jl
    deleted rotations for now in compute_psi matrix

--- src/states/wavefunctions.jl
+++ src/states/wavefunctions.jl
@@ -193,21 +193,17 @@ function wavefunction(state::S; b=5.0, inside_only=true, fundamental_domain = tr
         Psi2d::Array{type,2} = reshape(Psi, (nx,ny))
         if ~fundamental_domain 
             if ~isnothing(symmetries)
-                if all([symmetry isa Reflection for symmetry in symmetries])
-                    x_axis = 0.0
-                    y_axis = 0.0
-                    if hasproperty(billiard, :x_axis)
-                        #println(nameof(typeof(billiard)), " has the :x_axis reflection")
-                        x_axis = billiard.x_axis
-                    end
-                    if hasproperty(billiard, :y_axis)
-                        #println(nameof(typeof(billiard)), " has the :y_axis reflection")
-                        y_axis = billiard.y_axis
-                    end
-                    Psi2d, x_grid, y_grid = reflect_wavefunction(Psi2d,x_grid,y_grid,symmetries; x_axis=x_axis, y_axis=y_axis)
-                else # We have rotation
-                    Psi2d, x_grid, y_grid = rotate_wavefunction(Psi2d, x_grid, y_grid, symmetries[1], billiard)
+                x_axis = 0.0
+                y_axis = 0.0
+                if hasproperty(billiard, :x_axis)
+                    #println(nameof(typeof(billiard)), " has the :x_axis reflection")
+                    x_axis = billiard.x_axis
                 end
+                if hasproperty(billiard, :y_axis)
+                    #println(nameof(typeof(billiard)), " has the :y_axis reflection")
+                    y_axis = billiard.y_axis
+                end
+                Psi2d, x_grid, y_grid = reflect_wavefunction(Psi2d,x_grid,y_grid,symmetries; x_axis=x_axis, y_axis=y_axis)
             end
         end
         return Psi2d, x_grid, y_grid
