e6392e166e8624adc12237aa5dc180b9c08ba209
MathiasTensor <tensor1995@gmail.com>
2024-10-07 11:08:46 +0200

    Update plottingmakie.jl

--- src/plotting/plottingmakie.jl
+++ src/plotting/plottingmakie.jl
@@ -502,7 +502,7 @@ function plot_point_distribution!(f::Figure, billiard::Bi, solver::Sol; plot_idx
             arrows!(ax,getindex.(pts,1),getindex.(pts,2), getindex.(ns,1),getindex.(ns,2), color = :black, lengthscale = 0.1)
         end
         if !(samplers[i] isa LinearNodes) # avoid 0 range for colorbar for linear samplers
-            Colorbar(f[1, 2][1, i+j], sc)
+            Colorbar(f[1, 2][1, i+j], sc, label="$(i)")
             j += 1 
         end
     end
