95a9252f4b7f52988d5dc60bd40ddf2f3bca4cde
MathiasTensor <tensor1995@gmail.com>
2024-10-16 14:50:16 +0200

    Added localization measures
    Added localization measures and the inverse participation ratio calculation

---
+++ src/spectra/spectral_measures.jl
@@ -0,0 +1,39 @@
+
+
+# We will use the convention of using the 2πħ=1 like in Lozej, Batistić, Robnik. The same holds for the classical phase space volume for counting the number of the classical chaotic domain, so Nc=classical_phase_space_vol
+"""
+    localization_entropy(H::Matrix{T}, classical_phase_space_vol::T) where {T<:Real}
+
+Calculates the localization entropy of a quantum eigenstate's Husimi matrix. It uses the convention 2πħ=1 like in Lozej, Batistić, Robnik. The same holds for the classical phase space volume for counting the number of the classical chaotic domain, so Nc=classical_phase_space_vol.
+
+# Arguments
+- `H::Matrix`: The Husimi matrix of the quantum eigenstate
+- `classical_phase_space_vol`: The volume of the classical phase space.
+
+# Returns
+- `A<:Real`: The localization entropy A of the quantum eigenstate
+"""
+function localization_entropy(H::Matrix, classical_phase_space_vol::T) where {T<:Real}
+    H = H ./ sum(H) # normalize H
+    lnH = log.(H)
+    Im = sum(broadcast(*, H, lnH))
+    A = 1.0/classical_phase_space_vol*exp(-Im)
+    return T(A)
+end
+
+"""
+    normalized_inverse_participation_ratio_R(H::Matrix)
+
+Calculates the normalized inverse participation ratio R.
+
+# Arguments
+- `H::Matrix`: The Husimi matrix of the quantum eigenstate
+
+# Returns
+- `R<:Real`: The normalized inverse participation ratio R of the quantum eigenstate
+"""
+function normalized_inverse_participation_ratio_R(H::Matrix)
+    H = H ./ sum(H)
+    R = 1/(prod(size(H))*sum(H.^2)) # the prod(size(H)) is the grid count directly from the size of the matrix
+    return R
+end
