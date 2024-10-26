e6637a7bc9ce86457219429e5285d641caf52c12
MathiasTensor <tensor1995@gmail.com>
2024-10-26 13:46:11 +0200

    Update m_index.jl

--- src/spectra/m_index.jl
+++ src/spectra/m_index.jl
@@ -429,6 +429,9 @@ function separate_regular_and_chaotic_states(
         println("Theoretical ρ_reg: $(round(ρ_regular_classic, digits=6))")
         relative_closeness = abs(ρ_numeric_reg - ρ_regular_classic) / ρ_regular_classic * 100
         println("Relative closeness: $(round(relative_closeness, digits=4))%")
+        if relative_closeness < 2
+            break
+        end
 
         # Adjust decrease_step_size if ρ_numeric_reg has gone below ρ_regular_classic
         if ρ_numeric_reg < ρ_regular_classic
