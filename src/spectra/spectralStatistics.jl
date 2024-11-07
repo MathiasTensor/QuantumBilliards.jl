010415ccf52762edd5d5b525a58e9060985114a3
MathiasTensor <tensor1995@gmail.com>
2024-11-07 09:53:41 +0100

    Update spectralStatistics.jl

--- src/spectra/spectralStatistics.jl
+++ src/spectra/spectralStatistics.jl
@@ -92,11 +92,11 @@ function probability_berry_robnik_brody(s::T, rho::T, β::T) where {T<:Real}
     Γ_factor = gamma(1 + 1 / (1 + β))
     C2 = Γ_factor^(β + 1)
     T1 = (1 / Γ_factor) * exp(-rho * s)
-    T2 = real(Complex((1 / s) * exp((-1 + rho) * s * ((s - rho * s)^β * C2))))
+    T2 = (1 / s) * exp((-1 + rho) * s * (real(Complex((s - rho * s)))^β * C2))
     # Separate out complex components to prevent negative powers leading to complex values
-    inner_term = -((-1 + rho) * s * (s - rho * s)^β * C2)
+    inner_term = -((-1 + rho) * s * real(Complex((s - rho * s)))^β * C2)
     T3 = inner_term^(1 / (1 + β))
-    T4 = 2 * rho - (1 + β) * (-1 + rho) * (s - rho * s)^β * C2
+    T4 = 2 * rho - (1 + β) * (-1 + rho) * real(Complex((s - rho * s)))^β * C2
     T5 = (rho^2 * gamma(1 / (1 + β), inner_term)) / (1 + β)
     return T1 * (T2 * T3 * T4 + T5)
 end
