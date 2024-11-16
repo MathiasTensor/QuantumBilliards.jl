098a270e0bbbc976124dee4fa3a9208ef4dc3357
MathiasTensor <tensor1995@gmail.com>
2024-11-16 13:01:57 +0100

    Update classical_transport_time.jl

--- src/spectra/classical_transport_time.jl
+++ src/spectra/classical_transport_time.jl
@@ -183,7 +183,8 @@ function calculate_p2_averages(p_vals_all::Vector{Vector{T}}) where {T<:Real}
 end
 
 # SIMPLE PLOTTING WRAPPER
-function plot_p2_stats!(ax::Axis, p2_averages::Vector{T}, N_collisions::Int; window_size::Int=100) where {T<:Real}
+function plot_p2_stats!(ax::Axis, p2_averages::Vector{T}; window_size::Int=100) where {T<:Real}
+    N_collisions = length(p2_averages)
     actual_window_size = N_collisions < window_size ? 1 : window_size
     n = length(p2_averages)
     p2_averages_smoothed = [mean(p2_averages[i:min(i+actual_window_size-1, n)]) for i in 1:actual_window_size:n]
