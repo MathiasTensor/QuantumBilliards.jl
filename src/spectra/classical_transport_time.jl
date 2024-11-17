c3126886d40e961171f93bca9bcb5e667d5488fa
MathiasTensor <tensor1995@gmail.com>
2024-11-17 20:20:25 +0100

    Update classical_transport_time.jl

--- src/spectra/classical_transport_time.jl
+++ src/spectra/classical_transport_time.jl
@@ -215,7 +215,9 @@ function plot_p2_stats!(ax::Axis, p2_averages::Vector{T}; window_size::Int=1, lo
         scatter!(inset_ax, inset_iterations, inset_p2_averages, markersize=4, color=:blue)
         ylims!(inset_ax, 0.75*maximum(inset_p2_averages), 1.2*maximum(inset_p2_averages))
         xlims!(inset_ax, 0.0, inset_iterations[end])
+        L_range = collect(range(0, inset_iterations_limit, 10))
         #inset_ax.xtickformat = "{:.0f}"
+        inset_ax.xticklabelrotation=pi/2
         #hidedecorations!(inset_ax, grid=false)
     end
 end
