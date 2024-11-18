1e57fd307791d15dab2c6bf755b89bf1782a14b2
MathiasTensor <tensor1995@gmail.com>
2024-11-18 08:18:07 +0100

    Update otoc.jl
    Removed unneccesery code that was unoptimized for constructing the wavefucntions due to broadcasting

--- src/spectra/otoc.jl
+++ src/spectra/otoc.jl
@@ -56,44 +56,6 @@ Constructs a sequence of 2D wavefunctions as matrices over the same sized grid f
 - `y_grid::Vector{T}`: Vector of y-coordinates for the grid.
 """
 function wavefunction_multi(ks::Vector{T}, vec_us::Vector{Vector{T}}, vec_bdPoints::Vector{BoundaryPoints{T}}, billiard::Bi; b::Float64=5.0, inside_only::Bool=true) where {Bi<:AbsBilliard,T<:Real}
-    #=
-    k_max = maximum(ks) # for most resolution
-    type = eltype(k_max)
-    L = billiard.length
-    xlim,ylim = boundary_limits(billiard.full_boundary; grd=max(1000,round(Int, k_max*L*b/(2*pi))))
-    dx = xlim[2] - xlim[1]
-    dy = ylim[2] - ylim[1]
-    nx = max(round(Int, k_max*dx*b/(2*pi)), 512)
-    ny = max(round(Int, k_max*dy*b/(2*pi)), 512)
-    x_grid::Vector{type} = collect(type,range(xlim... , nx))
-    y_grid::Vector{type} = collect(type,range(ylim... , ny))
-    sz = length(x_grid)*length(y_grid)
-    pts = collect(SVector(x,y) for y in y_grid for x in x_grid)
-    if inside_only
-        pts_mask = points_in_billiard_polygon(pts, billiard, round(Int, sqrt(sz)); fundamental_domain=inside_only)
-        pts = pts[pts_mask]
-    end
-    # wavefunction via boundary integral Ψ = 1/4∮Yₒ(k|q-qₛ|)u(s)ds
-    function ϕ(x,y,k,bdPoints::BoundaryPoints, us::Vector)
-        target_point = SVector(x, y)
-        distances = norm.(Ref(target_point) .- bdPoints.xy)      
-        weighted_bessel_values = Bessels.bessely0.(k * distances) .* us .* bdPoints.ds  
-        return sum(weighted_bessel_values) / 4                  
-    end
-    Psi2ds = Vector{Matrix{type}}(undef, length(ks))
-    progress = Progress(length(ks), desc = "Constructing wavefunction matrices...")
-    Threads.@threads for i in eachindex(ks) 
-        k = ks[i]
-        bdPoints = vec_bdPoints[i]
-        us = vec_us[i]
-        Psi_flat = [ϕ(x,y,k,bdPoints,us) for y in y_grid for x in x_grid]  # Flattened vector
-        Psi_flat .= ifelse.(pts_mask, Psi_flat, zero(type))  # Zero out points outside billiard
-        Psi2ds[i] = reshape(Psi_flat, ny, nx)  # Reshape back to 2D matrix
-        next!(progress)
-    end
-    return Psi2ds, x_grid, y_grid
-    =#
-    # Precompute grid and mask
     k_max = maximum(ks)
     type = eltype(k_max)
     L = billiard.length
@@ -150,43 +112,6 @@ Constructs a sequence of 2D wavefunctions as matrices over the same sized grid f
 - `qs_list::Vector{Vector{T}}`: Vector of qs grids for the husimi matrices.
 """
 function wavefunction_multi_with_husimi(ks::Vector{T}, vec_us::Vector{Vector{T}}, vec_bdPoints::Vector{BoundaryPoints{T}}, billiard::Bi; b::Float64=5.0, inside_only::Bool=true) where {Bi<:AbsBilliard,T<:Real}
-    #=
-    k_max = maximum(ks) # for most resolution
-    type = eltype(k_max)
-    L = billiard.length
-    xlim,ylim = boundary_limits(billiard.full_boundary; grd=max(1000,round(Int, k_max*L*b/(2*pi))))
-    dx = xlim[2] - xlim[1]
-    dy = ylim[2] - ylim[1]
-    nx = max(round(Int, k_max*dx*b/(2*pi)), 512)
-    ny = max(round(Int, k_max*dy*b/(2*pi)), 512)
-    x_grid::Vector{type} = collect(type,range(xlim... , nx))
-    y_grid::Vector{type} = collect(type,range(ylim... , ny))
-    sz = length(x_grid)*length(y_grid)
-    pts = collect(SVector(x,y) for y in y_grid for x in x_grid)
-    if inside_only
-        pts_mask = points_in_billiard_polygon(pts, billiard, round(Int, sqrt(sz)); fundamental_domain=inside_only)
-        pts = pts[pts_mask]
-    end
-    # wavefunction via boundary integral Ψ = 1/4∮Yₒ(k|q-qₛ|)u(s)ds
-    function ϕ(x,y,k,bdPoints::BoundaryPoints, us::Vector)
-        target_point = SVector(x, y)
-        distances = norm.(Ref(target_point) .- bdPoints.xy)      
-        weighted_bessel_values = Bessels.bessely0.(k * distances) .* us .* bdPoints.ds  
-        return sum(weighted_bessel_values) / 4                  
-    end
-    Psi2ds = Vector{Matrix{type}}(undef, length(ks))
-    progress = Progress(length(ks), desc = "Constructing wavefunction matrices...")
-    Threads.@threads for i in eachindex(ks) 
-        k = ks[i]
-        bdPoints = vec_bdPoints[i]
-        us = vec_us[i]
-        Psi_flat = [ϕ(x,y,k,bdPoints,us) for y in y_grid for x in x_grid]  # Flattened vector
-        Psi_flat .= ifelse.(pts_mask, Psi_flat, zero(type))  # Zero out points outside billiard
-        Psi2ds[i] = reshape(Psi_flat, ny, nx)  # Reshape back to 2D matrix
-        next!(progress)
-    end
-    =#
-    # Precompute grid and mask
     k_max = maximum(ks)
     type = eltype(k_max)
     L = billiard.length
