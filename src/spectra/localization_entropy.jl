911dce8c244534f683f37819bd18c752191a7af1
MathiasTensor <tensor1995@gmail.com>
2024-11-20 11:17:44 +0100

    Update localization_entropy.jl

--- src/spectra/localization_entropy.jl
+++ src/spectra/localization_entropy.jl
@@ -314,9 +314,9 @@ function heatmap_M_vs_A_2d(
         # Randomly choose an angle for label offset
         #angle = rand() * 2π
         if isodd(j)
-            angle = 2*pi/3 + j/length(selected_indices)*pi
+            angle = 2*pi/3
         else
-            angle = -pi/3 + j/length(selected_indices)*pi
+            angle = -pi/3
         end
         # Set fixed distance for label offset
         label_distance = 0.08 * sqrt((maximum(As_bin_centers) - minimum(As_bin_centers))^2 + (maximum(Rs_bin_centers) - minimum(Rs_bin_centers))^2)
