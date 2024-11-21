cce581be54ee8932829cd211017b1e22a6261971
MathiasTensor <tensor1995@gmail.com>
2024-11-21 16:02:59 +0100

    Update otoc.jl

--- src/spectra/otoc.jl
+++ src/spectra/otoc.jl
@@ -1094,7 +1094,7 @@ function plot_wavefunctions_with_husimi(ks::Vector, Psi2ds::Vector, x_grid::Vect
             col = 1
         end
     end
-    resize_to_layout!(f)
+    #resize_to_layout!(f)
     return f
 end
 
