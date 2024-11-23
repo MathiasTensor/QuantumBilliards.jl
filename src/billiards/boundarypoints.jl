70c436f6b7dba9013e9bdce2e33ad1f0c0112d3a
MathiasTensor <tensor1995@gmail.com>
2024-11-24 00:05:19 +0100

    Update boundarypoints.jl
    bugfix - incorrect logic

--- src/billiards/boundarypoints.jl
+++ src/billiards/boundarypoints.jl
@@ -122,27 +122,24 @@ end
 
 #### ADDITION FOR BOUNDARY FUNCTIONS #####
 # Since the default one just uses the full boundary
-function boundary_coords_desymmetrized_full_boundary(billiard::Bi, sampler::S, N) where {Bi<:AbsBilliard, S<:AbsSampler}
+function boundary_coords_desymmetrized_full_boundary(billiard::Bi, sampler::FourierNodes, N) where {Bi<:AbsBilliard}
     let boundary = billiard.desymmetrized_full_boundary
-            L = symmetry_accounted_fundamental_boundary_length(boundary)
-            Lc = boundary[1].length
-            Nc = round(Int, N*Lc/L)
-            xy_all, normal_all, s_all, ds_all = boundary_coords(boundary[1],sampler,Nc)
-            #println(s_all)
-            l = boundary[1].length #cumulative length
-            for crv in boundary[2:end]
-                if (typeof(crv) <: AbsRealCurve )
-                    Lc = crv.length
-                    Nc = round(Int, N*Lc/L)
-                    xy,nxy,s,ds = boundary_coords(crv,sampler,Nc)
-                    append!(xy_all, xy)
-                    append!(normal_all, nxy)
-                    s = s .+ l
-                    append!(s_all, s)
-                    append!(ds_all, ds)
-                    l += Lc
-                end    
-            end
+        ts, dts = sample_points(sampler, N)
+        xy_all, normal_all, s_all, ds_all = boundary_coords(boundary[1], ts[1], dts[1])
+        l = boundary[1].length
+        for i in 2:length(ts)
+            crv = boundary[i]
+            if (typeof(crv) <: AbsRealCurve)
+                Lc = crv.length
+                xy,nxy,s,ds = boundary_coords(crv, ts[i], dts[i])
+                append!(xy_all, xy)
+                append!(normal_all, nxy)
+                s = s .+ l
+                append!(s_all, s)
+                append!(ds_all, ds)
+                l += Lc
+            end    
+        end
         return BoundaryPoints(xy_all,normal_all,s_all,ds_all) 
     end
 end
