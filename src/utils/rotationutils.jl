8e107732a2955b6cc77877cc7ae0bb1eeb9231c7
MathiasTensor <tensor1995@gmail.com>
2024-11-25 11:30:34 +0100

    Update rotationutils.jl

--- src/utils/rotationutils.jl
+++ src/utils/rotationutils.jl
@@ -334,27 +334,33 @@ function apply_symmetries_to_boundary_points(pts::BoundaryPoints{T}, symmetries:
                 combined_normal = vcat(shifted_normal, vertical_reflected_normal)
                 combined_s = vcat(full_s, vertical_reflected_s)
                 combined_ds = vcat(full_ds, vertical_reflected_ds)
+
                 # Reflect the combined result across the horizontal axis (y-axis reflection at y = y_axis)
                 horizontal_reflected_xy = [SVector(v[1], -v[2]) for v in combined_xy]
                 horizontal_reflected_normal = [SVector(n[1], -n[2]) for n in combined_normal]
-                horizontal_reflected_s = 2*(2*max_s) .- combined_s
+                horizontal_reflected_s = 2 * max_s .+ combined_s
                 horizontal_reflected_ds = combined_ds
+
                 # Resolve direction if `same_direction=true` for horizontal reflection
                 if same_direction
                     horizontal_reflected_xy = reverse(horizontal_reflected_xy)
                     horizontal_reflected_normal = reverse(horizontal_reflected_normal)
                 end
+
+                # Combine the vertical and horizontal reflections
                 final_xy = vcat(vertical_reflected_xy, horizontal_reflected_xy)
                 final_normal = vcat(vertical_reflected_normal, horizontal_reflected_normal)
                 final_s = vcat(vertical_reflected_s, horizontal_reflected_s)
                 final_ds = vcat(vertical_reflected_ds, horizontal_reflected_ds)
+
                 # Shift back reflected coordinates to the original alignment
                 shifted_back_xy = [SVector(v[1] + x_axis, v[2] + y_axis) for v in final_xy]
+
                 # Update reflected values
                 reflected_xy = shifted_back_xy
                 reflected_normal = final_normal
                 reflected_s = final_s
-                reflected_ds = final_ds     
+                reflected_ds = final_ds  
             end
             
             # combine the reflected components with the originals
