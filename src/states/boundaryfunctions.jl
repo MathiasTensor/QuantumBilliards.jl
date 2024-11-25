bb38e6ddbaab1557e1854487e22850837593a9bb
MathiasTensor <tensor1995@gmail.com>
2024-11-25 14:37:17 +0100

    Update boundaryfunctions.jl

--- src/states/boundaryfunctions.jl
+++ src/states/boundaryfunctions.jl
@@ -234,7 +234,7 @@ function boundary_function(state_data::StateData, billiard::Bi, basis::Ba; b=5.0
         println("length of vec before resize_basis call: ", dim)
         println("dimension of basis before resize_basis call: ", basis.dim)
         new_basis = resize_basis(basis, billiard, dim, ks[i])
-        println("length of vec after resize_basis call: ", new_basis.dim)
+        println("dimension of basis after resize_basis call: ", new_basis.dim)
         state = Eigenstate(ks[i], vec, tens[i], new_basis, billiard)
         u, s, norm = boundary_function(state; b=b)
         us[i] = u
