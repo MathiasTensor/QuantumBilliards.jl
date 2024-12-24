2b2bf5115e6185e5196aba3bcad3237dbd4abdab
MathiasTensor <tensor1995@gmail.com>
2024-12-24 12:52:46 +0100

    Update boundaryintegralmethod.jl

--- src/solvers/sweepmethods/boundaryintegralmethod.jl
+++ src/solvers/sweepmethods/boundaryintegralmethod.jl
@@ -1031,9 +1031,9 @@ function solve(solver::ExpandedBoundaryIntegralMethod,basis::Ba,pts::BoundaryPoi
     VR=VR[:,valid] # already normalized
     VL=VL[:,valid] # already normalized
     corr_1=-λ # consistency with taylor expansion expression A * u = - λ * B * u
-    denominators = real.(sum(conj(VL) .* (dA * VR), dims=1))[:]  # Flatten to 1D
+    denominators = real.(sum(VL .* (dA * VR), dims=1))[:]  # Flatten to 1D
     println("typeof denominators: ", typeof(denominators))
-    numerators = real.(sum(conj(VL) .* (ddA * VR), dims=1))[:]  # Flatten to 1D 
+    numerators = real.(sum(VL .* (ddA * VR), dims=1))[:]  # Flatten to 1D 
     println("typeof numerators: ", typeof(numerators))
     corr_2=-0.5*corr_1.^2 .* real.(numerators./denominators)
     println("typeof corr_2: ", typeof(corr_2))
