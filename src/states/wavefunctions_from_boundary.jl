7610304ce8c113d61afd6a3650c4f5384c2c6641
MathiasTensor <tensor1995@gmail.com>
2024-12-28 18:44:17 +0100

    Update wavefunctions_from_boundary.jl

--- src/states/wavefunctions_from_boundary.jl
+++ src/states/wavefunctions_from_boundary.jl
@@ -68,7 +68,7 @@ function wavefunction_multi(ks::Vector{T}, vec_us::Vector{Vector{T}}, vec_bdPoin
     pts_masked_indices=findall(pts_mask)
     Psi2ds=Vector{Matrix{type}}(undef,length(ks))
     progress=Progress(length(ks),desc="Constructing wavefunction matrices...")
-    Threads.@threads for i in eachindex(ks)
+    for i in eachindex(ks)
         k,bdPoints,us=ks[i],vec_bdPoints[i],vec_us[i]
         Psi_flat=zeros(type,sz)
         @inbounds for idx in pts_masked_indices # no bounds checking
