d3db50dc27710cecc47f311c3f9899c4f155048d
MathiasTensor <tensor1995@gmail.com>
2025-01-13 14:48:58 +0100

    Cleaning up unused code

--- src/utils/savingutils.jl
+++ src/utils/savingutils.jl
@@ -1,34 +1,5 @@
 using DataFrames, CSV,  PrettyTables
 
-
-#=
-DrWatson._wsave(filename, df::DataFrame; kwargs...) = Arrow.write(filename, df; kwargs...)
-
-function save_kspectrum(spect_data, params; folder="spectra")
-    output_stream = datadir(folder, savename("kspectrum", params, "arrow"; sigdigits = 4))
-    #println(output_stream)
-    df_spect =  DataFrame(k=spect_data.k, ten=spect_data.ten, control=spect_data.control)
-    safesave(output_stream, df_spect)
-end
-
-
-function load_kspectrum(params; folder="spectra")
-    output_stream = datadir(folder, savename("kspectrum", params, "arrow"; sigdigits = 4))
-    #println(output_stream)
-    df_spect =  DataFrame(k=spect_data.k, ten=spect_data.ten, control=spect_data.control)
-    safesave(output_stream, df_spect)
-end
-=#
-
-
-
-
-######## NEW ADDITIONS #########
-
-
-
-
-
 """
     save_numerical_ks_and_tensions!(ks::Vector{T}, tens::Vector{T}, filename::String) where {T<:Real}
 
