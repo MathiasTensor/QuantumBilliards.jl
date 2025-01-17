1b593786832ac4c275a14436c35546330a09bb10
MathiasTensor <tensor1995@gmail.com>
2025-01-17 11:13:50 +0100

    Update eigenstates.jl
    More precise documentation for computing eigenstate

--- src/states/eigenstates.jl
+++ src/states/eigenstates.jl
@@ -25,14 +25,14 @@ Constructs an `Eigenstate` object representing a stationary state of the wavefun
 # Returns
 An `Eigenstate` object with normalized coefficients.
 """
-function Eigenstate(k, vec, ten, basis, billiard)  
-    eps = set_precision(vec[1])
+function Eigenstate(k,vec,ten,basis,billiard)  
+    eps=set_precision(vec[1])
     if eltype(vec) <: Real
-        filtered_vec = eltype(vec).([abs(v)>eps ? v : zero(vec[1]) for v in vec])
+        filtered_vec=eltype(vec).([abs(v)>eps ? v : zero(vec[1]) for v in vec])
     else 
-        filtered_vec = vec
+        filtered_vec=vec
     end
-    return Eigenstate(k, k, filtered_vec,ten, length(vec), eps, basis, billiard)
+    return Eigenstate(k,k,filtered_vec,ten,length(vec),eps,basis,billiard)
 end
 
 """
@@ -51,14 +51,14 @@ Constructs an `Eigenstate` object, allowing for separate wavenumbers for the eig
 # Returns
 An `Eigenstate` object with normalized coefficients.
 """
-function Eigenstate(k, k_basis, vec, ten, basis, billiard)  
-    eps = set_precision(vec[1])
+function Eigenstate(k,k_basis,vec,ten,basis,billiard)  
+    eps=set_precision(vec[1])
     if eltype(vec) <: Real
-        filtered_vec = eltype(vec).([abs(v)>eps ? v : zero(vec[1]) for v in vec])
+        filtered_vec=eltype(vec).([abs(v)>eps ? v : zero(vec[1]) for v in vec])
     else 
-        filtered_vec = vec
+        filtered_vec=vec
     end
-    return Eigenstate(k, k_basis, filtered_vec, ten, length(vec), eps, basis, billiard)
+    return Eigenstate(k,k_basis,filtered_vec,ten,length(vec),eps,basis,billiard)
 end
 
 """
@@ -75,19 +75,19 @@ Computes a single eigenstate for a given wavenumber `k`.
 # Returns
 An `Eigenstate` object representing the computed eigenstate.
 """
-function compute_eigenstate(solver::SweepSolver, basis::AbsBasis, billiard::AbsBilliard,k)
-    L = billiard.length
-    dim = max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
-    basis_new = resize_basis(basis,billiard, dim, k)
-    pts = evaluate_points(solver, billiard, k)
-    ten, vec = solve_vect(solver, basis_new, pts, k)
-    return Eigenstate(k, vec, ten, basis_new, billiard)
+function compute_eigenstate(solver::SweepSolver,basis::AbsBasis,billiard::AbsBilliard,k)
+    L=billiard.length
+    dim=max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
+    basis_new=resize_basis(basis,billiard,dim,k)
+    pts=evaluate_points(solver,billiard,k)
+    ten,vec=solve_vect(solver,basis_new,pts,k)
+    return Eigenstate(k,vec,ten,basis_new,billiard)
 end
 
 """
     compute_eigenstate(solver::AcceleratedSolver, basis::AbsBasis, billiard::AbsBilliard, k::T; dk::T=0.1) where {T<:Real}
 
-Computes a single eigenstate for a given wavenumber `k` using an accelerated solver.
+Computes a single eigenstate for a given wavenumber `k` using an accelerated solver. Based on the inputted k it finds the closest one. In principle this will find an even better precide eigenvalue k for which we get it's basis coefficients expansion (`Eigenstate`).
 
 # Arguments
 - `solver::AcceleratedSolver`: The solver object to compute the eigenstate.
@@ -99,17 +99,17 @@ Computes a single eigenstate for a given wavenumber `k` using an accelerated sol
 # Returns
 An `Eigenstate` object representing the computed eigenstate.
 """
-function compute_eigenstate(solver::AcceleratedSolver, basis::AbsBasis, billiard::AbsBilliard, k; dk = 0.1)
-    L = billiard.length
-    dim = max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
-    basis_new = resize_basis(basis,billiard,dim,k)
-    pts = evaluate_points(solver, billiard, k)
-    ks, tens, X = solve_vectors(solver,basis_new, pts, k, dk)
-    idx = findmin(abs.(ks.-k))[2]
-    k_state = ks[idx]
-    ten = tens[idx]
-    vec = X[:,idx]
-    return Eigenstate(k_state, k, vec, ten, basis_new, billiard)
+function compute_eigenstate(solver::AcceleratedSolver,basis::AbsBasis,billiard::AbsBilliard,k;dk=0.1)
+    L=billiard.length
+    dim=max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
+    basis_new=resize_basis(basis,billiard,dim,k)
+    pts=evaluate_points(solver,billiard,k)
+    ks,tens,X=solve_vectors(solver,basis_new,pts,k,dk)
+    idx=findmin(abs.(ks.-k))[2]
+    k_state=ks[idx]
+    ten=tens[idx]
+    vec=X[:,idx]
+    return Eigenstate(k_state,k,vec,ten,basis_new,billiard)
 end
 
 struct EigenstateBundle{K,T,Ba,Bi} <: AbsState 
@@ -139,15 +139,15 @@ Constructs an `EigenstateBundle` object representing multiple eigenstates arisin
 # Returns
 An `EigenstateBundle` object containing the computed eigenstates.
 """
-function EigenstateBundle(ks, k_basis, X, tens, basis, billiard)  
-    eps = set_precision(X[1,1])
-    type = eltype(X)
-    if  type <: Real
-        filtered_array = type.([abs(x)>eps ? x : zero(type) for x in X])
+function EigenstateBundle(ks,k_basis,X,tens,basis,billiard)  
+    eps=set_precision(X[1,1])
+    type=eltype(X)
+    if type <: Real
+        filtered_array=type.([abs(x)>eps ? x : zero(type) for x in X])
     else 
-        filtered_array = X
+        filtered_array=X
     end
-    return EigenstateBundle(ks, k_basis, filtered_array, tens, length(X[:,1]), eps, basis, billiard)
+    return EigenstateBundle(ks,k_basis,filtered_array,tens,length(X[:,1]),eps,basis,billiard)
 end
 
 """
@@ -167,16 +167,16 @@ Computes a bundle of eigenstates within a small interval `[k-dk, k+dk]`.
 An `EigenstateBundle` object containing the computed eigenstates within the specified interval.
 """
 function compute_eigenstate_bundle(solver::AcceleratedSolver, basis::AbsBasis, billiard::AbsBilliard, k; dk = 0.1, tol=1e-5)
-    L = billiard.length
-    dim = max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
-    basis_new = resize_basis(basis,billiard, dim,k)
-    pts = evaluate_points(solver, billiard, k)
-    ks, tens, X = solve_vectors(solver,basis_new, pts, k, dk)
-    idx = abs.(tens) .< tol
-    ks = ks[idx]
-    tens = tens[idx]
-    X = X[:,idx]
-    return EigenstateBundle(ks, k, X, tens, basis_new, billiard)
+    L=billiard.length
+    dim=max(solver.min_dim,round(Int,L*k*solver.dim_scaling_factor/(2*pi)))
+    basis_new=resize_basis(basis,billiard, dim,k)
+    pts=evaluate_points(solver, billiard, k)
+    ks,tens,X=solve_vectors(solver,basis_new,pts,k,dk)
+    idx= abs.(tens) .< tol
+    ks=ks[idx]
+    tens=tens[idx]
+    X=X[:,idx]
+    return EigenstateBundle(ks,k,X,tens,basis_new,billiard)
 end
 
 # no need for basis and billiard data due to complex nested hierarchy
@@ -202,19 +202,17 @@ Constructor for the convenience wrapper `StateData`. Under the hood it filters t
 - `X::Vector{Matrix}`: The expansion coefficients for the basis stored as a Vector of vectors.
 - `tens::Vector`: The tension minima for which the wavefunction was computed.
 """
-function StateData(ks::Vector, X::Vector{Matrix}, tens::Vector)  
+function StateData(ks::Vector,X::Vector{Matrix},tens::Vector)  
     # Access the first element of the first vector in X
-    eps = set_precision(X[1][1])
-    type = eltype(X[1])
+    eps=set_precision(X[1][1])
+    type=eltype(X[1])
     if type <: Real
-        filtered_array = [
-            [abs(x) > eps ? x : zero(type) for x in vec] for vec in X
-        ] # Filter each vector in X individually
+        filtered_array=[[abs(x) > eps ? x : zero(type) for x in vec] for vec in X] # Filter each vector in X individually
     else
-        filtered_array = X
+        filtered_array=X
     end
     # dim can be gained for each k in ks separately as they all do not have the same dimension as the X vector of vectors has a different dimension for each k
-    return StateData(ks, filtered_array, tens)
+    return StateData(ks,filtered_array,tens)
 end
 
 # this is basically the new solve where we incur the smallest penalty for getting the ks and the relevant state information for saving the husimi functions but it is much more efficient than doint it again once we have the eigenvalues
@@ -233,13 +231,13 @@ Solves the generalized eigenvalue problem in a small interval `[k0-dk, k0+dk]` a
 # Returns
 A `StateData` object containing the wavenumbers, the tensions and the expansion coefficients for the basis stored as a Vector of Vectors after a generalized eigenvalue problem computation.
 """
-function solve_state_data_bundle(solver::Sol, basis::Ba, billiard::Bi, k, dk) where {Sol<:AbsSolver, Ba<:AbsBasis, Bi<:AbsBilliard}
-    L = billiard.length
-    dim = max(solver.min_dim,round(Int, L*k*solver.dim_scaling_factor/(2*pi)))
-    basis_new = resize_basis(basis,billiard, dim,k)
-    pts = evaluate_points(solver, billiard, k)
-    ks, tens, X_matrix = solve_vectors(solver,basis_new, pts, k, dk) # this one filters the ks that are outside k+-dk and gives us the filtered out ks, tensions and X matrix of filtered vectors. No need to store dim as we can get it from the length(X[1])
+function solve_state_data_bundle(solver::Sol,basis::Ba,billiard::Bi,k,dk) where {Sol<:AbsSolver, Ba<:AbsBasis, Bi<:AbsBilliard}
+    L=billiard.length
+    dim=max(solver.min_dim,round(Int,L*k*solver.dim_scaling_factor/(2*pi)))
+    basis_new=resize_basis(basis,billiard,dim,k)
+    pts=evaluate_points(solver,billiard, k)
+    ks,tens,X_matrix=solve_vectors(solver,basis_new,pts,k,dk) # this one filters the ks that are outside k+-dk and gives us the filtered out ks, tensions and X matrix of filtered vectors. No need to store dim as we can get it from the length(X[1])
     # Extract columns of X_matrix and store them as a Vector of Vectors b/c it is easier to merge them in the top function -> compute_spectrum_with_state
-    X_vectors = [Vector(col) for col in eachcol(X_matrix)]
-    return StateData(ks, X_vectors, tens)
+    X_vectors=[Vector(col) for col in eachcol(X_matrix)]
+    return StateData(ks,X_vectors,tens)
 end
