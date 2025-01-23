b39c9fbb8c5b7e3a502408d826b005ca3b08856e
MathiasTensor <tensor1995@gmail.com>
2025-01-23 14:54:43 +0100

    Update otoc.jl

--- src/spectra/otoc.jl
+++ src/spectra/otoc.jl
@@ -12,7 +12,7 @@ include("../states/wavefunctions.jl")
     wavefunction_normalized_multi_flat(ks::Vector{T}, vec_us::Vector{Vector{T}}, vec_bdPoints::Vector{BoundaryPoints{T}}, billiard::Bi;
                             b::Float64=5.0, inside_only::Bool=true, fundamental=true)
 
-Calculates the wavefunctions for a set of wavenumbers `ks` at points inside the billiard boundary and returns them as a vector of flat wavefunctions (normalized).
+Calculates the wavefunctions for a set of wavenumbers `ks` at points inside the billiard boundary and returns them as a vector of wavefunction matrices (normalized).
 
 # Arguments
 - `ks::Vector{T}`: Wavenumbers for which wavefunctions are computed.
@@ -22,8 +22,7 @@ Calculates the wavefunctions for a set of wavenumbers `ks` at points inside the 
 - `b::Float64`: Parameter controlling grid density (default = 5.0).
 
 # Returns
-- `Vector{Vector{T}}`: Vector of wavefunctions as flat arrays evaluated at points inside the billiard boundary.
-- `Vector{Matrix{T}}`: Vector of wavefunctions matrices on the grid, only if `return_full_wavefunctions` is true.
+- `Vector{Matrix{T}}`: Vector of wavefunctions matrices on the grid.
 - `x_grid::Vector{T}`: The grid on which the wavefunction is contructed
 - `Vector{SVector{2,T}}`: Points inside the billiard boundary.
 - `dx::T`: the x grid spacing, used for approximating intergrals into sums.
@@ -43,32 +42,23 @@ function wavefunction_normalized_multi_flat(ks::Vector{T},vec_us::Vector{Vector{
     # Determine points inside the billiard only once if `inside_only` is true
     pts_mask=points_in_billiard_polygon(pts,billiard,round(Int,sqrt(sz));fundamental_domain=false)
     pts_masked_indices=findall(pts_mask)
-    pts_inside=pts[pts_mask]
     # Prepare storage for wavefunctions
-    Psi_vectors=Vector{Vector{type}}(undef,length(ks))
-    Psi_matrices_full=Vector{Matrix{type}}(undef,length(ks))
+    Psi2ds=Vector{Matrix{type}}(undef,length(ks))
     progress=Progress(length(ks),desc="Constructing wavefunctions ...")
-    # Precompute coordinates of masked points
-    pts_inside_x=getindex.(pts_inside,1)
-    pts_inside_y=getindex.(pts_inside,2)
     # Compute wavefunctions
     Threads.@threads for i in eachindex(ks)
         k,bdPoints,us=ks[i],vec_bdPoints[i],vec_us[i]
-        Psi_flat=zeros(type,length(pts_inside))
-        Psi_flat_full=zeros(type,sz)
-        @inbounds for j in 1:length(pts_inside)
-            val=ϕ(pts_inside_x[j],pts_inside_y[j],k,bdPoints, us)
-            Psi_flat[j]=val
-            Psi_flat_full[pts_masked_indices[j]]=val
+        Psi_flat=zeros(type,sz)
+        @inbounds for idx in pts_masked_indices # no bounds checking
+            x,y=pts[idx]
+            Psi_flat[idx]=ϕ(x,y,k,bdPoints,us)
         end
         normalization=sum(Psi_flat)
-        Psi_flat./=normalization
-        Psi_vectors[i]=Psi_flat
         Psi_flat_full./=normalization
-        Psi_matrices_full[i]=reshape(Psi_flat_full,ny,nx)
+        Psi2ds[i]=reshape(Psi_flat_full,ny,nx)
         next!(progress)
     end
-    return Psi_vectors,Psi_matrices_full,x_grid,y_grid,pts_inside,dx,dy
+    return Psi2ds,x_grid,y_grid,pts_inside,dx,dy
 end
 
 """
