b74744fb6a68aeb73465f90e6737a97a362179c4
MathiasTensor <tensor1995@gmail.com>
2025-02-10 17:22:21 +0100

    Update acceleratedmethods.jl
    Added docstrings to the acceleratedmethods.jl

--- src/solvers/acceleratedmethods/acceleratedmethods.jl
+++ src/solvers/acceleratedmethods/acceleratedmethods.jl
@@ -1,23 +1,50 @@
-#include("../../abstracttypes.jl")
-#include("../../utils/billiardutils.jl")
-#include("decompositions.jl")
-#include("../samplers.jl")
 include("scalingmethod.jl")
 
-function solve_wavenumber(solver::AcceleratedSolver,basis::AbsBasis, billiard::AbsBilliard, k, dk)
-    dim = max(solver.min_dim,round(Int, billiard.length*k*solver.dim_scaling_factor/(2*pi)))
-    new_basis = resize_basis(basis,billiard,dim,k)
-    pts = evaluate_points(solver, billiard, k)
-    ks, ts = solve(solver,new_basis,pts,k,dk)
-    idx = findmin(abs.(ks.-k))[2]
-    return ks[idx], ts[idx]
+"""
+    solve_wavenumber(solver::AcceleratedSolver,basis::AbsBasis,billiard::AbsBilliard,k::T,dk::T) where {T<:Real}
+
+Solves the wavenumber for an `AcceleratedSolver` by finding the one closest to the given reference wavenumber `k` if found in the `dk` interval.
+
+# Arguments
+- `solver<:AcceleratedSolver`: An instance of `AcceleratedSolver`.
+- `basis<:AbsBasis`: The basis of the problem, no additional restrictions.
+- `billiard<:AbsBilliard`: billiard instance, geometrical information.
+- `k::T`: The reference wavenumber.
+- `dk::T`: The interval in which to find the closest wavenumber.
+
+# Returns
+- `T`: The closest wavenumber found in the `dk` interval.
+- `T`: The corresponding tension found for the closest wavenumber (by construction smallest tension in the given interval via findmin in the code).
+"""
+function solve_wavenumber(solver::AcceleratedSolver,basis::AbsBasis,billiard::AbsBilliard,k,dk)
+    dim=max(solver.min_dim,round(Int,billiard.length*k*solver.dim_scaling_factor/(2*pi)))
+    new_basis=resize_basis(basis,billiard,dim,k)
+    pts=evaluate_points(solver,billiard,k)
+    ks,ts=solve(solver,new_basis,pts,k,dk)
+    idx=findmin(abs.(ks.-k))[2]
+    return ks[idx],ts[idx]
 end
 
+"""
+    solve_spectrum(solver::AcceleratedSolver,basis::AbsBasis,billiard::AbsBilliard,k::T,dk::T) where {T<:Real}
 
-function solve_spectrum(solver::AcceleratedSolver,basis::AbsBasis, billiard::AbsBilliard, k, dk)
-    dim = max(solver.min_dim,round(Int, billiard.length*k*solver.dim_scaling_factor/(2*pi)))
-    new_basis = resize_basis(basis,billiard,dim,k)
-    pts = evaluate_points(solver, billiard, k)
-    ks, ts = solve(solver,new_basis,pts,k,dk)
-    return ks, ts
+Solves for all the wavenumbers and corresponding tensions that `solve(<:AcceleratedSolver...)` gives us in the given interval `dk`.
+
+# Arguments
+- `solver<:AcceleratedSolver`: An instance of `AcceleratedSolver`.
+- `basis<:AbsBasis`: The basis of the problem, no additional restrictions.
+- `billiard<:AbsBilliard`: billiard instance, geometrical information.
+- `k::T`: The reference wavenumber.
+- `dk::T`: The interval in which to find the wavenumbers and corresponding tensions.
+
+# Returns
+- `Vector{T}`: The wavenumbers found in the `dk` interval.
+- `Vector{T}`: The corresponding tensions found for the wavenumbers in the `dk` interval.
+"""
+function solve_spectrum(solver::AcceleratedSolver,basis::AbsBasis,billiard::AbsBilliard,k,dk)
+    dim=max(solver.min_dim,round(Int,billiard.length*k*solver.dim_scaling_factor/(2*pi)))
+    new_basis=resize_basis(basis,billiard,dim,k)
+    pts=evaluate_points(solver, billiard,k)
+    ks,ts=solve(solver,new_basis,pts,k,dk)
+    return ks,ts
 end
