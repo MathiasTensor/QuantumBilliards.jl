5ab75ecb39583bd0f8659a6ad48d7ee3a1cd103b
MathiasTensor <tensor1995@gmail.com>
2025-03-17 16:08:16 +0100

    Update QuantumBilliards.jl

--- src/QuantumBilliards.jl
+++ src/QuantumBilliards.jl
@@ -60,7 +60,7 @@ export adapt_basis
 #include("limacon.jl")
 #include("rectangle.jl")
 export Stadium, Lemon, Triangle, Sinai
-export curve, tangent, normal, arc_length
+export curve, tangent, arc_length
 export tangent_vec, normal_vec
 #convenience functions may be moved somewhere else
 #export make_stadium_and_basis, make_triangle_and_basis 
