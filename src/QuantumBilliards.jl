8f220485ecf10a868a734182d05e02c20583ccde
MathiasTensor <tensor1995@gmail.com>
2025-03-17 16:10:27 +0100

    Update QuantumBilliards.jl

--- src/QuantumBilliards.jl
+++ src/QuantumBilliards.jl
@@ -145,7 +145,7 @@ export benchmark_solver, compute_benchmarks
 include("plotting/benchmarkplotting.jl")
 export plot_benchmarks!
 include("utils/savingutils.jl")
-export save_numerical_ks_and_tensions!, read_numerical_ks_and_tensions, compute_and_save_closest_pairs!, plot_and_save_eigenstate_results!, save_numerical_ks!, read_numerical_ks, filter_and_save_ks_and_tensions!, plot_Z!, dynamical_solver_construction, save_ks_and_husimi_functions!, read_ks_and_husimi_functions
+export save_numerical_ks_and_tensions!, read_numerical_ks_and_tensions, compute_and_save_closest_pairs!, save_numerical_ks!, read_numerical_ks, filter_and_save_ks_and_tensions!, plot_Z!, dynamical_solver_construction, save_ks_and_husimi_functions!, read_ks_and_husimi_functions
 include("utils/rotationutils.jl")
 include("spectra/m_index.jl")
 export visualize_overlap, compute_M, shift_s_vals_poincare_birkhoff, classical_phase_space_matrix, visualize_quantum_classical_overlap_of_levels!, plot_hist_M_distribution!, compute_overlaps, separate_ks_by_classical_indices, fraction_of_mixed_states, get_mixed_states, coefficient_of_fraction_of_mixed_eigenstates_vs_k, plot_fraction_of_mixed_eigenstates_vs_k, separate_Hs_by_classical_indices, separate_by_classical_indices, separate_ks_and_Hs_by_classical_indices, compute_fractions_of_mixed_eigenstates, plot_fraction_mixed_states, visualize_husimi_and_wavefunction!, save_separation_parameters!, load_separation_parameters, get_mixed_states_boolean_mask
