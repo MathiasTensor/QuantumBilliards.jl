d90f70977d760d850a0c10e190ab38ed9f5071ea
MathiasTensor <tensor1995@gmail.com>
2025-04-11 11:05:21 +0200

    Update macros.jl

--- src/utils/macros.jl
+++ src/utils/macros.jl
@@ -1,19 +1,22 @@
+"""
+    use_threads(args...)
+    
+The macro expects either:
+(a) A keyword argument "multithreading" followed by a loop expression, or
+b) A lone loop expression (in which case multithreading defaults to true).
+"""
 macro use_threads(args...)
-    # The macro expects either:
-    #   (a) A keyword argument "multithreading" followed by a loop expression, or
-    #   (b) A lone loop expression (in which case multithreading defaults to true).
-    if length(args) >= 2 && args[1] isa Expr && args[1].head == :(=) && args[1].args[1] == :multithreading
-        if length(args) != 2
+    if length(args)>=2 && args[1] isa Expr && args[1].head==:(=) && args[1].args[1]==:multithreading
+        if length(args)!=2
             error("Usage: @use_threads multithreading=[true|false] for ...")
         end
         # Extract the provided multithreading value and the loop expression.
-        multithreading_val = args[1].args[2]
-        loop_expr = args[2]
-
+        multithreading_val=args[1].args[2]
+        loop_expr=args[2]
         # If the provided value is literally true or false, we branch accordingly at compile time.
-        if multithreading_val === true
+        if multithreading_val===true
             return esc(:(@inbounds Base.Threads.@threads $loop_expr))
-        elseif multithreading_val === false
+        elseif multithreading_val===false
             return esc(:(@inbounds $loop_expr))
         else
             # If not a literal (i.e. it's some expression that will be evaluated at runtime),
@@ -28,9 +31,9 @@ macro use_threads(args...)
                 end
             end)
         end
-    elseif length(args) == 1
+    elseif length(args)==1
         # No keyword argument provided. Default behavior is multithreading=true.
-        loop_expr = args[1]
+        loop_expr=args[1]
         return esc(:(@inbounds Base.Threads.@threads $loop_expr))
     else
         error("Usage: @use_threads [multithreading=[true|false]] for ...")
