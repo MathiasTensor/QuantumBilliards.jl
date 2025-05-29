1fe541afb23ca3d60a5840aa9496d8644ac3f6ac
MathiasTensor <tensor1995@gmail.com>
2025-05-29 11:31:43 +0200

    Update localization_entropy.jl

--- src/spectra/localization_entropy.jl
+++ src/spectra/localization_entropy.jl
@@ -698,8 +698,9 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
             xticklabelsvisible=false,
             yticklabelsvisible=false
         )
+        H_bg ./= maximum(H_bg)
+        heatmap!(ax_husimi, chaotic_mask; colormap=cgrad([:white, :black]), alpha=0.02, colorrange=(0, 1))
         heatmap!(ax_husimi, H_bg; colormap=Reverse(:gist_heat), colorrange=(0.0, maximum(H_bg)))
-        heatmap!(ax_husimi, chaotic_mask; colormap=cgrad([:white, :black]), alpha=0.05, colorrange=(0, 1))
         text!(ax_husimi, 0.5, 0.1, text=roman_label, color=:black, fontsize=10)
         col += 1
         if col > 4
