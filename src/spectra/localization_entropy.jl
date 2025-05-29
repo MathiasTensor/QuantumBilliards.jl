582b190da04e637ff4fe462c4e10f7194b00ce09
MathiasTensor <tensor1995@gmail.com>
2025-05-29 19:02:51 +0200

    Update localization_entropy.jl

--- src/spectra/localization_entropy.jl
+++ src/spectra/localization_entropy.jl
@@ -605,7 +605,7 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
     fig = Figure(resolution=(3000, 2500), size=(3000, 2500))  # Adjusted size for three plots
 
     ### Top Plot: P(A, M) ###
-    ax_top = Axis(fig[1, 1][1, 1], title="P(A, M)", xlabel="A", ylabel="M", xtickformat="{:.2f}", ytickformat="{:.2f}", xlabelsize=35, ylabelsize=35, xticklabelsize=30, yticklabelsize=30)
+    ax_top = Axis(fig[1, 1][1, 1], title="P(A, M)", xlabel="A", ylabel="M", xtickformat="{:.2f}", ytickformat="{:.2f}", xlabelsize=40, ylabelsize=40, xticklabelsize=30, yticklabelsize=30)
     #ax_top.xticks = range(min_A, max_A, 20) 
     ax_top.yticks = range(M_min, M_max, 10) 
     ax_top.xticklabelrotation = pi/2
@@ -642,11 +642,11 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
         # Plot a black square marker (outline) at the data point with transparent fill
         scatter!(ax_top, [A], [M], marker=:rect, color=:transparent, markersize=8, strokecolor=:black, strokewidth=3.5)
         # Place the text inside the square
-        text!(ax_top, A, M, text=roman_label, color=:black, fontsize=40, halign=:center, valign=:center)
+        text!(ax_top, A, M, text=roman_label, color=:black, fontsize=45, halign=:center, valign=:center)
     end
 
     ### Middle Plot: P(A, R) ###
-    ax_middle = Axis(fig[1, 1][1, 2], title="P(A, R)", xlabel="A", ylabel="R", xtickformat="{:.2f}", ytickformat="{:.2f}", xlabelsize=35, ylabelsize=35, xticklabelsize=30, yticklabelsize=30)
+    ax_middle = Axis(fig[1, 1][1, 2], title="P(A, R)", xlabel="A", ylabel="R", xtickformat="{:.2f}", ytickformat="{:.2f}", xlabelsize=40, ylabelsize=40, xticklabelsize=30, yticklabelsize=30)
     #ax_middle.xticks = range(min_A, max_A, 20) 
     ax_middle.yticks = range(R_min, R_max, 10) 
     ax_middle.xticklabelrotation = pi/2
@@ -687,7 +687,7 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
 
     ### Husimi Functions at the Bottom ###
     # Husimi function grid layout at the bottom
-    husimi_grid = fig[2:3, 1] = GridLayout()
+    husimi_grid = fig[2, 1] = GridLayout()
     row = 1
     col = 1
     for (j, selected_index) in enumerate(selected_indices)
@@ -708,7 +708,7 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
             ygridvisible=false,
             xticklabelsvisible=false,
             yticklabelsvisible=false,
-            titlesize=35
+            titlesize=45
         )
         H_bg ./= maximum(H_bg)
         Threads.@threads for i in axes(H_bg,1)
