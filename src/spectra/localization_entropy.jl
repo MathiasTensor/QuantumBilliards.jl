770bf5f96f19c3a2bee68c3cb3ab179f7f618dd8
MathiasTensor <tensor1995@gmail.com>
2025-05-29 17:53:39 +0200

    Update localization_entropy.jl

--- src/spectra/localization_entropy.jl
+++ src/spectra/localization_entropy.jl
@@ -710,16 +710,16 @@ function combined_heatmaps_with_husimi(Hs_list::Vector, qs_list::Vector, ps_list
             yticklabelsvisible=false
         )
         H_bg ./= maximum(H_bg)
-        Threads.@threads for i in size(H_bg,1)
-            for j in size(H_bg,2)
-                if projection_grid[i,j] == -1 # if it is regular
-                    H_bg[i,j] = NaN 
-                else # the chaotic region
+        Threads.@threads for i in axes(H_bg,1)
+            for j in axes(H_bg,2)
+                if projection_grid[i,j] == 1 # if it is chaotic
+                    H_bg[i,j] = ifelse(H_bg[i,j] > 1e-2, H_bg[i,j], NaN) 
+                else # the regular region
                     H_bg[i,j] = H_bg[i, j] 
                 end
             end
         end
-        heatmap!(ax_husimi, H_bg; colormap=Reverse(:gist_heat), colorrange=(0.0, 1.0), nan_color=:gray)
+        heatmap!(ax_husimi, H_bg; colormap=Reverse(:gist_heat), colorrange=(0.0, 1.0), nan_color=:lightgray)
         # Label
         text!(ax_husimi, 0.5, 0.1, text=roman_label, color=:black, fontsize=10)
         col += 1
