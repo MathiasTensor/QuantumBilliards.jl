3f73454252eddb2f8283babc468accbd2391adc4
MathiasTensor <tensor1995@gmail.com>
2025-06-02 15:21:49 +0200

    Update testing.jl

--- src/test/testing.jl
+++ src/test/testing.jl
@@ -160,13 +160,12 @@ end
     k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
     ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
     sort!(ks_analytical)
-    ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
 
     billiard,_=make_rectangle_and_basis(w,h)
     fem=QuantumBilliards.FiniteElementMethod(billiard,200,200;k_max=1000.0)
     nev=3
     Es,wavefunctions=QuantumBilliards.compute_fem_eigenmodes(fem,nev=nev,maxiter=100000,tol=1e-8)
-    ks=sqrt.(abs.(2 .*Es))
+    ks=sqrt.(abs.(Es))
     println(ks)
     println(ks_analytical)
     @test all(k->any(ka->abs(ka-k)≤1e-2,ks_analytical),ks) 
