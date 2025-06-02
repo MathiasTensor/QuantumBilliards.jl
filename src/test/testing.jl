6f5248e0956596115bca9ed92b3d0f61d6f55e69
MathiasTensor <tensor1995@gmail.com>
2025-06-02 11:48:22 +0200

    Update testing.jl

--- src/test/testing.jl
+++ src/test/testing.jl
@@ -4,6 +4,13 @@ using Test
 Contains tests for various methods for computing the eigenvalue. It is easiest to test for the accuracy of eigenvalues if integrable models like the rectangle or the circle.
 =#
 
+#### COMMON TO ALL TESTS ####
+k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
+ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
+sort!(ks_analytical)
+ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
+
+
 ###########################################
 #### Vergini Saraceno (ScalingMethodA) ####
 ###########################################
@@ -23,9 +30,30 @@ Contains tests for various methods for computing the eigenvalue. It is easiest t
     state_res,_=compute_spectrum_with_state(acc_solver,basis,billiard,k1,k2,dk)
     ks=state_res.ks
     # analytical data - for the odd-odd class we have m,n even
-    k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
-    ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
-    sort!(ks_analytical)
-    ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
     @test all(k->any(ka->abs(ka-k)≤1e-4,ks_analytical),ks) # check if all ks are in the analytical set up to 1e-4
+end
+
+
+#############################
+#### DecompositionMethod ####
+#############################
+
+
+@testset "DecompositionMethod" begin
+    k1=18.0
+    k2=20.0
+    kgrid=collect(range(k1,k2,step=1e-4))
+    w=2.0
+    h=1.0
+    d=5.0
+    b=10.0
+    threshold=200.0
+    billiard,basis=make_rectangle_and_basis(w,h)
+    samplers=[GaussLegendreNodes(),LinearNodes()]
+    for sampler in samplers 
+        dm=DecompositionMethod(d,b,sampler=sampler)
+        tens=k_sweep(dm,basis,billiard,kgrid)
+        ks=get_eigenvalues(kgrid,tens,threshold=threshold)
+        @test all(k->any(ka->abs(ka-k)≤1e-3,ks_analytical),ks) # check if all ks are in the analytical set up to 1e-3 (smaller than the grid spacing)
+    end
 end
