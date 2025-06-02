1a294b7472e8770272a0e0a5278b48ce128cb46f
MathiasTensor <tensor1995@gmail.com>
2025-06-02 12:34:20 +0200

    Update testing.jl

--- src/test/testing.jl
+++ src/test/testing.jl
@@ -43,7 +43,7 @@ end
 
     ebim_dk(k)=0.03*k^(-1/3)
     billiard,basis=make_rectangle_and_basis(w,h)
-    symmetries=Vector{Any}[XYReflection(-1,-1)]
+    symmetries=Vector{Any}([XYReflection(-1,-1)])
     ebim_solver=ExpandedBoundaryIntegralMethod(b,billiard,symmetries=symmetries)
     ks_all,_=compute_spectrum(ebim_solver,billiard,k1,k2,dk=ebim_dk,use_lapack_raw=true)
     @test all(k->any(ka->abs(ka-k)≤1e-3,ks_analytical),ks) # check if all ks are in the analytical set up to 1e-3
