8da28897614d44b9479e30ea296677424b0cdd66
MathiasTensor <tensor1995@gmail.com>
2025-06-02 11:54:29 +0200

    Update testing.jl

--- src/test/testing.jl
+++ src/test/testing.jl
@@ -4,18 +4,6 @@ using Test
 Contains tests for various methods for computing the eigenvalue. It is easiest to test for the accuracy of eigenvalues if integrable models like the rectangle or the circle.
 =#
 
-#### COMMON TO ALL TESTS ####
-w=2.0
-h=1.0
-d=5.0
-b=10.0
-k1=18.0
-k2=20.0
-k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
-ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
-sort!(ks_analytical)
-ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
-
 
 ###########################################
 #### Vergini Saraceno (ScalingMethodA) ####
@@ -23,6 +11,17 @@ ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of inte
 
 
 @testset "Vergini Saraceno (ScalingMethodA)" begin
+    w=2.0
+    h=1.0
+    d=5.0
+    b=10.0
+    k1=18.0
+    k2=20.0
+    k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
+    ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
+    sort!(ks_analytical)
+    ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
+
     dk=0.05
     billiard,basis=make_rectangle_and_basis(w,h)
     acc_solver=ScalingMethodA(d,b)
@@ -39,6 +38,17 @@ end
 
 
 @testset "DecompositionMethod" begin
+    w=2.0
+    h=1.0
+    d=5.0
+    b=10.0
+    k1=18.0
+    k2=20.0
+    k_analytical(_m,_n,_w,_h)=sqrt((_m*pi/_w)^2+(_n*pi/_h)^2)
+    ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
+    sort!(ks_analytical)
+    ks_analytical=filter(k->k1≤k≤k2,ks_analytical) # filter to the range of interest
+    
     kgrid=collect(range(k1,k2,step=1e-4))
     threshold=200.0
     billiard,basis=make_rectangle_and_basis(w,h)
