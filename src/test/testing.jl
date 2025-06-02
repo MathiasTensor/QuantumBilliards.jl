547fcaac8ab648d861076a714f591b2c41faec9f
MathiasTensor <tensor1995@gmail.com>
2025-06-02 14:57:38 +0200

    Update testing.jl

--- src/test/testing.jl
+++ src/test/testing.jl
@@ -137,8 +137,8 @@ end
     ks_analytical=[k_analytical(m,n,w,h) for m=0:10, n=0:10 if (m>0 && n>0)]
     billiard,_=make_rectangle_and_basis(w,h)
     fundamental=false
-    fem=QuantumBilliards.FiniteElementMethod(billiard,200,200;k_max=1000.0,offset_x_symmetric=0.1,offset_y_symmetric=0.1,fundamental=fundamental)
-    nev=10
+    fem=QuantumBilliards.FiniteElementMethod(billiard,250,250;k_max=1000.0,offset_x_symmetric=0.1,offset_y_symmetric=0.1,fundamental=fundamental)
+    nev=5
     γ=5.0
     σ=1.01
     Es,_=compute_ϕ_fem_eigenmodes(fem,phi,γ,σ,nev=nev,maxiter=50000,tol=1e-8)
