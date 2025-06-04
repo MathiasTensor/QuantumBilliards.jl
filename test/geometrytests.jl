43266e6d8860d3add0c114a548499af9da5c5cef
MathiasTensor <tensor1995@gmail.com>
2025-06-04 14:45:15 +0200

    Update geometrytests.jl

--- test/geometrytests.jl
+++ test/geometrytests.jl
@@ -4,6 +4,60 @@ using LinearAlgebra
 using SpecialFunctions    # for elliptic integrals
 using QuantumBilliards
 
+
+###########################################
+#### Line Segments and Circle Segments ####
+###########################################
+
+@testset "Stadium geometry" begin
+    a=1.0
+    r=1.0
+    stadium,_=QuantumBilliards.make_stadium_and_basis(a)
+    @test length(stadium.full_boundary)==4
+
+    # Test that the curve is closed: curve(t=0) ≈ curve(t=1)
+    P0=curve(stadium.full_boundary[1],zero(eltype(a)))
+    P1=curve(stadium.full_boundary[end],one(eltype(a)))
+    @test isapprox(P0,P1;atol=1e-8)
+
+    all_checks_normalization=Bool[]
+    all_checks_orthogonality=Bool[]
+    for seg in stadium.full_boundary
+        # Test that tangent‐vector norm is 1 after normalization
+        ts=range(0.0,1.0;length=20)
+        tanvecs=QuantumBilliards.tangent_vec(seg,collect(ts))
+        push!(all_checks_normalization,all([isapprox(norm(v),1.0;atol=1e-6) for v in tanvecs]))
+        # Test that normal_vec is orthogonal to tangent_vec at each sample
+        norvecs=QuantumBilliards.normal_vec(seg,collect(ts))
+        push!(all_checks_orthogonality,all([isapprox(abs(dot(tanvecs[i],norvecs[i])),0.0,rtol=1e-8) for i in 1:length(ts)]))
+    end
+    all_checks_normalization=vcat(all_checks_normalization...)
+    all_checks_orthogonality=vcat(all_checks_orthogonality...)
+    @test all(all_checks_normalization)
+    @test all(all_checks_orthogonality)
+
+    # Inside / outside pts check
+    pts=[SVector(0.0,0.0), SVector(1.0,0.0), SVector(0.0,2.0),SVector(-1.0,-0.5)]
+    checks=QuantumBilliards.points_in_billiard_polygon(pts,stadium,10000,fundamental_domain=false)
+    @test checks==[true,true,false,true]
+
+    # test boundary coords
+    N=10000
+    sampler=GaussLegendreNodes()
+    bp=QuantumBilliards.boundary_coords(stadium,sampler,N)
+    xy=bp.xy
+    normals=bp.normal
+    s_vals=bp.s
+    ds=bp.ds
+
+    @test length(xy)==length(normals)==length(s_vals)==length(ds)
+    @test isapprox(sum(ds),stadium.length;atol=1e-4)  # total ds should sum to L_numeric
+end
+
+#############################
+#### For the Polar Curve ####
+#############################
+
 # =============================================================================
 # Helper: Analytical ellipse perimeter using complete elliptic integral of the second kind:
 #   P = 4 a E(m), where m = 1 – (b/a)^2
