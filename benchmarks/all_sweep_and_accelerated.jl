c3aa8a4c2a39748ef7090bddc375862f22d6b7ac
MathiasTensor <tensor1995@gmail.com>
2025-06-04 16:54:24 +0200

    Update all_sweep_and_accelerated.jl
    Cleaned up the function that tests all the methods

--- benchmarks/all_sweep_and_accelerated.jl
+++ benchmarks/all_sweep_and_accelerated.jl
@@ -50,67 +50,66 @@ end
 #######################################################
 
 # Compute the analytical eigenvalues for a circle billiard using Bessel function zeros. Useful since in the spectrum_test function we can compare the numerical results with the analytical ones.
-function compute_circle_analytical_eigenvalues(k_min, k_max, radius)
-    analytical_eigenvalues = []  # List to hold eigenvalues (k_mn, nu, zero_index)
-    nu = 0
+function compute_circle_analytical_eigenvalues(k_min::Real,k_max::Real,radius::Real)
+    analytical_eigenvalues=[]  # List to hold eigenvalues (k_mn, nu, zero_index)
+    nu=0
     while true
-        stop_outer_loop = false
-        zero_index = 1
+        stop_outer_loop=false
+        zero_index=1
         while true
             # Compute the zero of the Bessel function J_nu
-            zero = besselj_zero(nu, zero_index)  # `bessel_zero` gives the `zero_index`th root of J_nu
-            k_mn = zero / radius
+            zero=besselj_zero(nu,zero_index)  # `bessel_zero` gives the `zero_index`th root of J_nu
+            k_mn=zero/radius
             # Stop conditions
-            if k_mn > k_max
-                if zero_index == 1
-                    stop_outer_loop = true
+            if k_mn>k_max
+                if zero_index==1
+                    stop_outer_loop=true
                 end
                 break
             end
-            if k_mn >= k_min
+            if k_mn>=k_min
                 # Append (k_mn, nu, zero_index)
-                push!(analytical_eigenvalues, (k_mn, nu, zero_index))
+                push!(analytical_eigenvalues,(k_mn,nu,zero_index))
                 # Add the negative nu case for doublets if nu > 0
-                if nu > 0
-                    push!(analytical_eigenvalues, (k_mn, -nu, zero_index))
+                if nu>0
+                    push!(analytical_eigenvalues,(k_mn,-nu,zero_index))
                 end
             end
-            zero_index += 1
+            zero_index+=1
         end
         if stop_outer_loop
             break
         end
-        nu += 1
+        nu+=1
     end
     return analytical_eigenvalues
 end
 
 # Compute the analytical eigenvalues for a rectangle billiard using the formula k_mn = sqrt((m * π / width)^2 + (n * π / height)^2). Useful since in the spectrum_test function we can compare the numerical results with the analytical ones.
 function compute_rectangle_analytical_eigenvalues(k_min::Real, k_max::Real, width::Real, height::Real) 
-    analytical_eigenvalues = Vector{Tuple{Float64, Int, Int}}()
-
-    m = 1
+    analytical_eigenvalues=Vector{Tuple{Float64,Int,Int}}()
+    m=1
     while true
-        stop_outer_loop = false
-        n = 1
+        stop_outer_loop=false
+        n=1
         while true
-            k_mn_squared = (m * π / width)^2 + (n * π / height)^2
-            if k_mn_squared > k_max^2
-                if n == 1
-                    stop_outer_loop = true
+            k_mn_squared=(m*π/width)^2+(n*π/height)^2
+            if k_mn_squared>k_max^2
+                if n==1
+                    stop_outer_loop=true
                 end
                 break
             end
-            if k_min^2 <= k_mn_squared
-                k_mn = sqrt(k_mn_squared)
-                push!(analytical_eigenvalues, (k_mn, m, n))
+            if k_min^2<=k_mn_squared
+                k_mn=sqrt(k_mn_squared)
+                push!(analytical_eigenvalues,(k_mn,m,n))
             end
-            n += 1
+            n+=1
         end
         if stop_outer_loop
             break
         end
-        m += 1
+        m+=1
     end
 
     return analytical_eigenvalues
@@ -137,101 +136,98 @@ fundamental: Boolean flag to indicate whether to compute the fundamental wavefun
 do_husimi: Boolean flag to indicate whether to compute and plot the Husimi plots. Defaults to true.
 =#
 function spectrum_test(k1,k2;make_movie=false,b=10.0,x=:D,y=:D,type=:xy,sampler_bim=[LinearNodes()],sampler_other=[GaussLegendreNodes()],dk=(k)->(0.05*k^(-1/3)),save_wavefunctions=false,do_standard=true,do_VS=true,raw_lapack=false,do_ebim=true,fundamental=false,do_husimi=true,plot_ebim_debug=false)
-    #billiard, basis = make_circle_and_basis(1.0)
-    #billiard, basis = make_stadium_and_basis(1.0)
-    #billiard, basis = make_prosen_and_basis(0.6)
-    #billiard, basis = make_ellipse_and_basis(2.0,1.0)
-    billiard, basis = make_robnik_and_basis(0.15)
-    #billiard, basis = make_rectangle_and_basis(2.0, 1.0)
-    #billiard, basis = make_equilateral_triangle_and_basis(1.0)
+    #billiard,basis=make_circle_and_basis(1.0)
+    #billiard,basis=make_stadium_and_basis(1.0)
+    #billiard,basis=make_prosen_and_basis(0.6)
+    #billiard,basis=make_ellipse_and_basis(2.0,1.0)
+    billiard,basis=make_robnik_and_basis(0.15)
+    #billiard,basis=make_rectangle_and_basis(2.0, 1.0)
+    #billiard,basis=make_equilateral_triangle_and_basis(1.0)
     #billiard,basis=make_generalized_sinai_and_basis()
-    #billiard, basis = make_mushroom_and_basis(0.5,1.0,1.0)
-    dm_solver = DecompositionMethod(5.0,8.0,sampler_other)
-    psm_solver = ParticularSolutionsMethod(5.0,8.0,8.0,sampler_other)
-    acc_solver = ScalingMethodA(5.0,8.0,sampler_other)
+    #billiard,basis=make_mushroom_and_basis(0.5,1.0,1.0)
+    dm_solver=DecompositionMethod(5.0,8.0,sampler_other)
+    psm_solver=ParticularSolutionsMethod(5.0,8.0,8.0,sampler_other)
+    acc_solver=ScalingMethodA(5.0,8.0,sampler_other)
     if type==:xy
         if x==:D && y==:D
-            symmetry = Vector{Any}([XYReflection(-1,-1)])
+            symmetry=Vector{Any}([XYReflection(-1,-1)])
         elseif x==:D && y==:N
-            symmetry = Vector{Any}([XYReflection(-1,1)])
+            symmetry=Vector{Any}([XYReflection(-1,1)])
         elseif x==:N && y==:D
-            symmetry = Vector{Any}([XYReflection(1,-1)])
+            symmetry=Vector{Any}([XYReflection(1,-1)])
         elseif x==:N && y==:N
-            symmetry = Vector{Any}([XYReflection(1,1)])
+            symmetry=Vector{Any}([XYReflection(1,1)])
         end
     elseif type==:x
         if x==:D
-            symmetry = Vector{Any}([XReflection(-1)])
+            symmetry=Vector{Any}([XReflection(-1)])
         elseif x==:N
-            symmetry = Vector{Any}([XReflection(1)])
+            symmetry=Vector{Any}([XReflection(1)])
         end
     elseif type==:y
         if y==:D
-            symmetry = Vector{Any}([YReflection(-1)])
+            symmetry=Vector{Any}([YReflection(-1)])
         elseif y==:N
-            symmetry = Vector{Any}([YReflection(1)])
+            symmetry=Vector{Any}([YReflection(1)])
         end
-    elseif type==:rot
+    elseif type==:rot # NOT IMPLEMENTED
         if x==:D
-            symmetry = Vector{Any}([QuantumBilliards.Rotation(3,-1)])
+            symmetry=Vector{Any}([QuantumBilliards.Rotation(3,-1)])
         elseif x==:N
-            symmetry = Vector{Any}([QuantumBilliards.Rotation(3,1)])
+            symmetry=Vector{Any}([QuantumBilliards.Rotation(3,1)])
         end
     else
         symmetry=nothing
     end
 
-    bim_solver = QuantumBilliards.BoundaryIntegralMethod(b, sampler_bim,billiard; symmetries=symmetry, x_bc=x, y_bc=y)
-    println("bim solver symmetry: ", bim_solver.rule)
-    ebim_solver = QuantumBilliards.ExpandedBoundaryIntegralMethod(b, sampler_bim,billiard; symmetries=symmetry, x_bc=x, y_bc=y)
-
-    symmetryBIM=QuantumBilliards.SymmetryRuleBIM(billiard; symmetries=symmetry, x_bc=x, y_bc=y)
-    println("Symmetry type: ", symmetryBIM.symmetry_type)
-    k_range = collect(range(k1,k2,step=0.0005))
+    bim_solver=QuantumBilliards.BoundaryIntegralMethod(b,sampler_bim,billiard;symmetries=symmetry,x_bc=x,y_bc=y)
+    println("bim solver symmetry: ",bim_solver.rule)
+    ebim_solver=QuantumBilliards.ExpandedBoundaryIntegralMethod(b,sampler_bim,billiard;symmetries=symmetry,x_bc=x,y_bc=y)
+    symmetryBIM=QuantumBilliards.SymmetryRuleBIM(billiard;symmetries=symmetry,x_bc=x,y_bc=y)
+    println("Symmetry type: ",symmetryBIM.symmetry_type)
+    k_range=collect(range(k1,k2,step=0.0005))
     k=(k1+k2)/2
     pts=QuantumBilliards.evaluate_points(bim_solver,billiard,k)
-
-    hankel_basis = QuantumBilliards.AbstractHankelBasis()
+    hankel_basis=QuantumBilliards.AbstractHankelBasis()
     if do_ebim
         # checks for indexes of singular distance behaviour in bim and ebim matrices (any off diagonal elements)
         #QuantumBilliards.distance_singular_check!(ebim_solver,billiard,k,num_print_idxs=10)
-        @time ks_ebim,tens_ebim = compute_spectrum(ebim_solver,billiard,k1,k2,dk=dk,use_lapack_raw=raw_lapack)
-        #@time ks_ebim,tens_ebim = QuantumBilliards.compute_spectrum_new(ebim_solver,billiard,k1,k2,dk=dk,use_lapack_raw=raw_lapack)
+        @time ks_ebim,tens_ebim=compute_spectrum(ebim_solver,billiard,k1,k2,dk=dk,use_lapack_raw=raw_lapack)
+        #@time ks_ebim,tens_ebim=QuantumBilliards.compute_spectrum_new(ebim_solver,billiard,k1,k2,dk=dk,use_lapack_raw=raw_lapack)
         @time ks_debug1,tens_debug1,ks_debug2,tens_debug2=QuantumBilliards.visualize_ebim_sweep(ebim_solver,hankel_basis,billiard,k1,k2;dk=dk)
     end
-    @time tens_bim = k_sweep(bim_solver,hankel_basis,billiard,k_range)
-    
+    @time tens_bim=k_sweep(bim_solver,hankel_basis,billiard,k_range)
     if do_standard
-        @time tens_dm = k_sweep(dm_solver,basis,billiard,k_range)
-        @time tens_psm = k_sweep(psm_solver,basis,billiard,k_range)
+        @time tens_dm=k_sweep(dm_solver,basis,billiard,k_range)
+        @time tens_psm=k_sweep(psm_solver,basis,billiard,k_range)
     end
-
     if do_VS
-        @time ks_VS,tens_VS = compute_spectrum(acc_solver,basis,billiard,k1,k2; N_expect = 1, dk_threshold=0.05)
+        @time ks_VS,tens_VS=compute_spectrum(acc_solver,basis,billiard,k1,k2,dk=0.1)
+        idxs=findall(x->x<1e-3,tens_VS) # all the correct eigenvalues
+        ks_VS=ks_VS[idxs]
+        tens_VS=tens_VS[idxs]
     end
-   
     f=Figure(size=(3500,2000),resolution=(3500,2000))
-    ax = Axis(f[1,1], title="$(nameof(typeof(billiard)))_$(nameof(typeof(basis)))_$(nameof(typeof(sampler_bim[1])))_$(x)$(y)", ylabel=L"log(f_μ(k))", xlabel=L"k")
-    lines!(ax,k_range,log10.(tens_bim), color=:blue)
+    ax=Axis(f[1,1],title="$(nameof(typeof(billiard)))_$(nameof(typeof(basis)))_$(nameof(typeof(sampler_bim[1])))_$(x)$(y)",ylabel=L"log(f_μ(k))", xlabel=L"k")
+    lines!(ax,k_range,log10.(tens_bim),color=:blue)
     if do_standard
-        lines!(ax,k_range,log10.(tens_dm), color=:red)
-        lines!(ax,k_range,log10.(tens_psm), color=:green)
+        lines!(ax,k_range,log10.(tens_dm),color=:red)
+        lines!(ax,k_range,log10.(tens_psm),color=:green)
     end
     if do_VS
-        scatter!(ax,ks_VS,log10.(tens_VS), color=:orange)
+        scatter!(ax,ks_VS,log10.(tens_VS),color=:orange)
     end
     if do_ebim
-        scatter!(ax,ks_ebim,log10.(tens_ebim), color=:black)
+        scatter!(ax,ks_ebim,log10.(tens_ebim),color=:black)
     end
     xlims!(ax,k1,k2)
-    
     # process the gradient
     mid_x,gradient=QuantumBilliards.bim_second_derivative(k_range,log10.(tens_bim))
     # these are the ks
     ks=QuantumBilliards.get_eigenvalues(k_range,tens_bim) # for easier logic
     #vlines!(ax,peaks,linewidth=0.5,color=:red)
     vlines!(ax,ks,linewidth=0.5,color=:red)
-    ax=Axis(f[2,1], title="$(nameof(typeof(billiard)))_$(nameof(typeof(basis)))_$(nameof(typeof(sampler_bim[1])))_$(x)$(y)_2nd_GRADIENT", ylabel=L"∂/∂k(log(f_μ(k)))", xlabel=L"k")
+    ax=Axis(f[2,1],title="$(nameof(typeof(billiard)))_$(nameof(typeof(basis)))_$(nameof(typeof(sampler_bim[1])))_$(x)$(y)_2nd_GRADIENT",ylabel=L"∂/∂k(log(f_μ(k)))",xlabel=L"k")
     lines!(ax,mid_x,gradient,color=:green)
     vlines!(ax,ks,linewidth=0.5)
     xlims!(ax,k1,k2)
@@ -246,47 +242,36 @@ function spectrum_test(k1,k2;make_movie=false,b=10.0,x=:D,y=:D,type=:xy,sampler_
         xlims!(ax,k1,k2)
         axislegend()
     end
-
     if do_ebim && do_VS && !isempty(ks_VS)
         function find_closest(ks_VS,ks_ebim)
             closest_values=Vector{Float64}(undef,length(ks_ebim))
             for i in eachindex(ks_ebim)
-                closest_index = argmin(abs.(ks_VS .- ks_ebim[i]))
-                closest_values[i] = ks_VS[closest_index]
+                closest_index=argmin(abs.(ks_VS.-ks_ebim[i]))
+                closest_values[i]=ks_VS[closest_index]
             end
             return closest_values
         end
-        closest_ks_VS = find_closest(ks_VS, ks_ebim)
+        closest_ks_VS=find_closest(ks_VS,ks_ebim)
         println("Comparison between EBIM and VS...")
         for i in eachindex(ks_ebim)
-            println("k EBIM: ", ks_ebim[i], " k VS: ", closest_ks_VS[i])
+            println("k EBIM: ",ks_ebim[i], " k VS: ",closest_ks_VS[i])
         end
     end
-
     save("$(nameof(typeof(billiard)))_spectrum_$(k1)_$(k2)_$(nameof(typeof(sampler_bim[1])))_b_$(b)_x_$(x)_y_$(y).png", f)
-
-    f = Figure(resolution=(4000,3000),size=(4000,3000))  # Define a 6x5 grid with proper resolution
-    r_max, c_max = 6, 5  # Number of rows and columns in the grid
+    f=Figure(resolution=(4000,3000),size=(4000,3000))  # Define a 6x5 grid with proper resolution
+    r_max,c_max=6,5  # Number of rows and columns in the grid
     r=1
     c=1
-    for k in ks[1:min(length(ks), r_max * c_max)] # ince bim gives useful info we can check the derivatives of the feedholm matrices as a gauge of the EBIM 
+    for k in ks[1:min(length(ks),r_max*c_max)] # ince bim gives useful info we can check the derivatives of the feedholm matrices as a gauge of the EBIM 
         # Axis for fredholm_matrix
-        ax1 = Axis(f[r,c], title = "k=$k Fredholm", aspect = DataAspect(), width=500, height=500)
-        heatmap!(ax1, log10.(abs.(QuantumBilliards.fredholm_matrix(
-            QuantumBilliards.evaluate_points(bim_solver, billiard, k), symmetryBIM, k
-        ))))
-        
+        ax1=Axis(f[r,c],title="k=$k Fredholm",aspect=DataAspect(),width=500,height=500)
+        heatmap!(ax1,log10.(abs.(QuantumBilliards.fredholm_matrix(QuantumBilliards.evaluate_points(bim_solver,billiard, k),symmetryBIM,k))))
         # Axis for fredholm_matrix_derivative
-        ax2 = Axis(f[r,c+1], title = "dA/dk k=$k", aspect = DataAspect(), width=500, height=500)
-        heatmap!(ax2, log10.(abs.(QuantumBilliards.fredholm_matrix_der(
-            QuantumBilliards.evaluate_points(bim_solver, billiard, k), symmetryBIM, k, kernel_fun=:first
-        ))))
+        ax2=Axis(f[r,c+1],title="dA/dk k=$k",aspect=DataAspect(),width=500,height=500)
+        heatmap!(ax2,log10.(abs.(QuantumBilliards.fredholm_matrix_der(QuantumBilliards.evaluate_points(bim_solver,billiard,k),symmetryBIM,k, kernel_fun=:first))))
         # Axis for fredholm_matrix_second_derivative
-        ax3 = Axis(f[r,c+2], title = "d^2A/dk^2 k=$k", aspect = DataAspect(), width=500, height=500)
-        heatmap!(ax3, log10.(abs.(QuantumBilliards.fredholm_matrix_der(
-            QuantumBilliards.evaluate_points(bim_solver, billiard, k), symmetryBIM, k, kernel_fun=:second
-        ))))
-        
+        ax3=Axis(f[r,c+2],title="d^2A/dk^2 k=$k",aspect=DataAspect(),width=500,height=500)
+        heatmap!(ax3,log10.(abs.(QuantumBilliards.fredholm_matrix_der(QuantumBilliards.evaluate_points(bim_solver,billiard,k),symmetryBIM,k,kernel_fun=:second))))
         if c>6
             c=1
             r+=1
@@ -295,15 +280,14 @@ function spectrum_test(k1,k2;make_movie=false,b=10.0,x=:D,y=:D,type=:xy,sampler_
         c+=3
     end
     save("$(nameof(typeof(billiard)))_A_dA_ddA_$(k1)_$(k2)_$(nameof(typeof(sampler_bim[1])))_b_$(b)_x_$(x)_y_$(y).png", f)
-
     # save wavefunctions and husimi
     if save_wavefunctions
         if do_ebim
             ks=ks_ebim
         end
         us_all,pts_all=QuantumBilliards.solve_eigenvectors_BIM(bim_solver,billiard,hankel_basis,ks)
-        println("Length of us_all: ", [length(us) for us in us_all])
-        println("Length of s all: ", [length(pts.ds) for pts in pts_all])
+        println("Length of us_all: ",[length(us) for us in us_all])
+        println("Length of s all: ",[length(pts.ds) for pts in pts_all])
         pts_all,us_all=QuantumBilliards.boundary_function_BIM(bim_solver,us_all,pts_all,billiard) # pts_all is of type BoundaryPoints
         s_vals_all=[pts.s for pts in pts_all]
         if do_husimi
@@ -315,10 +299,8 @@ function spectrum_test(k1,k2;make_movie=false,b=10.0,x=:D,y=:D,type=:xy,sampler_
         end
         save("$(nameof(typeof(billiard)))wavefunctions_$(k1)_$(k2)_$(nameof(typeof(sampler_bim[1])))_b_$(b)_x_$(x)_y_$(y).png", f)
     end
-
-    # make movie
     if make_movie
-        QuantumBilliards.create_fredholm_movie!(k_range,billiard; symmetries=Vector{Any}([XYReflection(-1,-1)]), sampler=[GaussLegendreNodes()], output_path="$(nameof(typeof(billiard)))_fredholm_evol_$(k1)_$(k2)_$(nameof(typeof(sampler_bim[1]))).mp4")
+        QuantumBilliards.create_fredholm_movie!(k_range,billiard;symmetries=Vector{Any}([XYReflection(-1,-1)]),sampler=[GaussLegendreNodes()],output_path="$(nameof(typeof(billiard)))_fredholm_evol_$(k1)_$(k2)_$(nameof(typeof(sampler_bim[1]))).mp4")
     end
 end
 
