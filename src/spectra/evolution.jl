4c916e1b834ebc79311413e5dc6e9a45584cf5a8
MathiasTensor <tensor1995@gmail.com>
2025-06-17 09:46:22 +0200

    Update evolution.jl

--- src/spectra/evolution.jl
+++ src/spectra/evolution.jl
@@ -299,8 +299,10 @@ function animate_wavepacket_evolution!(filename::String,coeffs_matrix::Matrix{Co
     Psi=sum(coeffs_matrix[1,j]*Psi2ds[j] for j in psi_idxs)
     hm_real=heatmap!(ax_real,x_grid,y_grid,abs.(Psi),colormap=:balance)
     ax_momentum=Axis(fig[1,2],title="Momentum Distribution",xlabel="kx",ylabel="ky")
-    kx_grid=fftshift(fftfreq(length(x_grid)))*(2π/(x_grid[end]-x_grid[1]))
-    ky_grid=fftshift(fftfreq(length(y_grid)))*(2π/(y_grid[end]-y_grid[1]))
+    #kx_grid=fftshift(fftfreq(length(x_grid)))*(2π/(x_grid[end]-x_grid[1]))
+    #ky_grid=fftshift(fftfreq(length(y_grid)))*(2π/(y_grid[end]-y_grid[1]))
+    kx_grid=fftshift(fftfreq(length(x_grid)),1/(x_grid[end]-x_grid[1]))*2π
+    ky_grid=fftshift(fftfreq(length(y_grid)),1/(y_grid[end]-y_grid[1]))*2π
     compute_momentum_distribution(Ψ_x)=abs2.(fftshift(fft(Ψ_x)))
     frames_real=Vector{Matrix{T}}(undef,length(ts))
     frames_momentum=Vector{Matrix{T}}(undef,length(ts))
