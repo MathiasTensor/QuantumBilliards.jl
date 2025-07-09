2f5fb7cff3ee3652e60db14eaea9eadab0d2e6f5
MathiasTensor <tensor1995@gmail.com>
2025-07-09 19:46:50 +0200

    Update CFIE.jl

--- src/solvers/sweepmethods/CFIE.jl
+++ src/solvers/sweepmethods/CFIE.jl
@@ -284,7 +284,6 @@ end
 ##############
 
 function solve(solver::CFIE{T},basis::Ba,pts::BoundaryPointsCFIE{T},k;use_combined::Bool=false) where {T<:Real,Ba<:AbstractHankelBasis}
-    pts=evaluate_points(solver,solver.billiard,k)
     N=length(pts.xy)
     Rmat=zeros(T,N,N)
     kress_R_fft!(Rmat) # or kress_R_sum!(Rmat) for small N
