79902d2782b12c58397b8c6b36329f85d91a053e
MathiasTensor <tensor1995@gmail.com>
2025-07-09 19:22:35 +0200

    Update CFIE.jl
    Main implementation of CFIE

--- src/solvers/sweepmethods/CFIE.jl
+++ src/solvers/sweepmethods/CFIE.jl
@@ -1,7 +1,15 @@
+##########################
+#### BESSEL FUNCTIONS ####
+##########################
+
+H(n::Int,x::T) where {T<:Real}=Bessels.hankelh1(n,x)
+J(n::Int,x::T) where {T<:Real}=Bessels.besselj(n,x)
+J(ns::As,x::T) where {T<:Real,As<:AbstractRange{Int}}=Bessels.besselj(ns,x)
 
 ##########################################################################################################
 #### WEIGHT FUNCTIONS USED BY KRESS: Boundary Integral Equations in time-harmonic acoustic scattering ####
 ##########################################################################################################
+
 # The parameter s ∈ [0,2π] in all non-reparametried functions. We rescale it since segment parametrizations go from [0,1]
 v(s::T,q::Int) where {T<:Real}=(1/q-1/2)*((pi-s)/pi)^3+1/q*((s-pi)/pi)+0.5
 dv(s::T,q::Int) where {T<:Real}=-(3*(1/q-1/2)/π)*((π-s)/π)^2+1/(q*π)
@@ -110,6 +118,176 @@ function evaluate_points(solver::CFIE,billiard::Bi,k) where {Bi<:AbsBilliard}
         append!(ak_all,ak)
     end
     return BoundaryPointsCFIE(xy_all,normal_all,kappa_all,sk_all,sk_local_all,ak_all)
+end
+
+##################################
+#### KRESS CIRCULANT R MATRIX ####
+##################################
+
+# Provide 2 functions, kress_R_fft! and kress_R_sum!, to compute the circulant R matrix for the Kress method. kress_R_fft! uses the FFT to compute the matrix efficiently, while kress_R_sum! computes it using a direct summation approach. Both functions modify the input matrix R0 in place. The best performance is achieved with kress_R_fft! for large matrices, while kress_R_sum! is more straightforward and may be easier to understand for smaller matrices.
+# Ref: Kress, R., Boundary integral equations in time-harmonic acoustic scattering. Mathematics Comput. Modelling Vol 15, pp. 229-243). Pergamon Press, 1991, GB.
+
+# Alex Barnett's idea to use ifft to get the circulant vector kernel and construct the circulant with circshift.
+function kress_R_fft!(R0::AbstractMatrix{T}) where {T<:Real}
+    N=size(R0,1)
+    n=N÷2
+    a=zeros(Complex{T},N) #  build the spectral vector a (first col)
+    for m in 1:(n-1)
+        a[m+1]=1/m     # positive freq
+        a[N-m+1]=1/m     # negative freq
+    end # leave a[n+1] == 0  (no 1/n term)
+    rjn=real(ifft(a)) # inverse FFT → rjn[j] = (2/N)*∑_{m=1..n-1} (1/m) cos(2π m (j-1)/N)
+    ks=0:(N-1) # build the first column, adding the “alternating” correction
+    alt=(-1).^ks # alt[j+1] = (-1)^j
+    @. R0[:,1]=-2*pi*rjn+(4*pi/(N^2))*alt # R0[:,1] = -2π*rjn .+ (4π/N^2)*alt, first col is ref
+    for j in 2:N # fill out the rest circulantly:
+        @views R0[:,j].=circshift(R0[:,j-1],1) # shift by +1 wrt previous column
+    end
+    return nothing
+end
+
+function kress_R_sum!(R0::AbstractMatrix{T}) where {T<:Real}
+    N=size(R0,1)
+    M=N/2-1
+    ks=collect(0:N-1) # build the 1d series s[0:N-1] 
+    s=zeros(T,N)
+    @inbounds for m in 1:M
+        s.+=(1/m).*cos.(2*pi*m.*ks./N)
+    end
+    alt=(-1).^ks # "alternating" term  cos((N/2)*(i-j)*2π/N) = (-1)^(i-j)
+    # build the index‐difference matrix once: idx[i,j] = mod(i-j, N) in 0:(N-1)
+    idx=@. mod((1:N)'.-(1:N),N)  # this is N×N of UInts
+    @. R0=-4*pi/N*(s[idx.+1].-(1/N)*alt[idx.+1]) # fill R0 with one big broadcast
+    return nothing
+end
+
+################################################################
+#### FIRST AND SECOND LAYER BOUNDARY POTENTIAL CONSTRUCTION ####
+################################################################
+
+#### ONLY NEED TO USE DOUBLE LAYER POTENTIAL - NO NEUMANN RESONANCES CHECK ####
+function L1_L2_matrix(pts::BoundaryPointsCFIE{T},k::T) where {T<:Real}
+    two_pi=2*pi
+    N=length(pts.xy)
+    X=getindex.(pts.xy,1)
+    Y=getindex.(pts.xy,2)
+    ΔX=@. X-X'   # ΔX[i,j] = X[i] - X[j] = x(t_i) - x(t_j)
+    ΔY=@. Y-Y'   # ΔY[i,j] = Y[i] - Y[j] = y(t_i) - y(t_j)
+    R=hypot.(ΔX,ΔY)
+    R[diagind(R)].=one(T) # avoid zeros on diagonal, does not influence result since overwritten few lines below
+    nxs=getindex.(pts.normal,1)
+    nys=getindex.(pts.normal,2)
+    dX=nys # tangent x
+    dY=-nxs # tangent y
+    κ=pts.curvature
+    Δs=@. pts.sk .-pts.sk'
+    dX_mat=reshape(dX,1,N)
+    dY_mat=reshape(dY,1,N)
+    inner=@. dY_mat*ΔX-dX_mat*ΔY
+    H1=zeros(Complex{T},size(R)) 
+    @inbounds for i in 1:N # In this case R is symmetric
+        H1[i,i]=one(T) # can be whatever
+        for j in i+1:N
+            H1ij=H(1,k*R[i,j])
+            H1[i,j]=H1ij
+            H1[j,i]=H1ij
+        end
+    end
+    J1=real.(H1)
+    # assemble L and L1 off the diagonal
+    L1=-k/(two_pi)*inner.*J1./R
+    L=im*k/2*inner.*H1./R
+    L2=L.-L1.*log.(4*sin.(Δs/2).^2)
+    # fix diagonal entries by taking the known limits
+    L1[diagind(L1)].=zero(Complex{T}) # lim t→s L1 = 0 for SLP
+    L2[diagind(L2)].=κ./(two_pi) # the diagona of SLP is 0 so no contribution with log(w'(s))L1(s,s)
+    return L1,L2
+end
+
+function L1_L2_M1_M2_matrix(pts::BoundaryPointsCFIE{T},k::T) where {T<:Real}
+    two_pi=2*pi
+    N=length(pts.xy)
+    X=getindex.(pts.xy,1)
+    Y=getindex.(pts.xy,2)
+    ΔX=@. X-X'   # ΔX[i,j] = X[i] - X[j] = x(t_i) - x(t_j)
+    ΔY=@. Y-Y'   # ΔY[i,j] = Y[i] - Y[j] = y(t_i) - y(t_j)
+    R=hypot.(ΔX,ΔY)
+    R[diagind(R)].=one(T) # avoid zeros on diagonal, does not influence result since overwritten few lines below
+    nxs=getindex.(pts.normal,1)
+    nys=getindex.(pts.normal,2)
+    dX=nys # tangent x
+    dY=-nxs # tangent y
+    κ=pts.curvature
+    Δs=@. pts.sk .-pts.sk'
+    dX_mat=reshape(dX,1,N)
+    dY_mat=reshape(dY,1,N)
+    inner=@. dY_mat*ΔX-dX_mat*ΔY
+    H1=zeros(Complex{T},size(R))
+    H0=zeros(Complex{T},size(R))  
+    @inbounds for i in 1:N # In this case R is symmetric
+        H1[i,i]=1.0 # can be whatever since diagonal limits correction later
+        H0[i,i]=1.0 # can be whatever since diagonal limits correction later
+        for j in i+1:N
+            H1ij=H(1,k*R[i,j])
+            H0ij=H(0,k*R[i,j])
+            H1[i,j]=H1ij
+            H1[j,i]=H1ij
+            H0[i,j]=H0ij
+            H0[j,i]=H0ij
+        end
+    end
+    # bessels as Re: Hj=Jj-im*Yj
+    J1=real.(H1)
+    J0=real.(H0)
+    speed=@. sqrt(dX^2+dY^2) 
+    speed_row=reshape(speed,1,N) # 1×N this should be [x'(τ)^2 + y'(τ) for τ is ts]
+    # assemble L and L1 off the diagonal element wise 
+    L1=-k/(2*pi)*inner.*J1./R
+    L=im*k/2*inner.*H1./R
+    L2=L.-L1.*log.(4*sin.(Δs/2).^2)
+    # assemble M1 and M2 off the diagonal element wise 
+    M1=-1/(two_pi).*J0.*speed_row
+    M=im/2. *H0.*speed_row
+    M2=M.-M1.*log.(4*sin.(Δs/2).^2)
+    # fix diagonal entries by taking the known limits
+    κlim=1/(two_pi)*κ # length-N
+    d=diagind(L1)
+    L1[d].=zero(Complex{T}) # lim t→s L1 = 0 for SLP
+    L2[d].=κlim # the curvature limit for DLP
+    M1[d].=-1/(two_pi).*speed
+    M2[d].=((im/2-MathConstants.eulergamma/pi).-(1/(two_pi)).*log.((k^2)/4 .*speed.^2)).*speed .+2 .*log.(pts.ak).*M1[d] # Kress's modification to DLP limit with 2*log(w'(s))*M1(s,s)
+    return L1,L2,M1,M2
+end
+
+#####################################
+#### Vectorized Nystrom M Matrix ####
+#####################################
+
+function M(pts::BoundaryPointsCFIE{T},k::T,Rmat::Matrix{T};use_combined::Bool=false) where {T<:Real}
+    if use_combined
+      L1,L2,M1,M2=L1_L2_M1_M2_matrix(pts,k)
+      A_double=(Rmat.*L1.+(2π/N).*L2).*pts.ak' # DLP case
+      A_single=(Rmat.*M1.+(2π/N).*M2).*pts.ak' # SLP case
+      A=A_double.+(im*k)*A_single # D+i*k*S
+    else
+      L1,L2=L1_L2_matrix(ts,k)
+      A=@. (Rmat.*L1.+(2π/N).*L2).*pts.ak' # just DLP case
+    end
+    return Diagonal(ones(Complex{T},N))-A
+end
+
+##############
+#### MAIN ####
+##############
+
+function solve(solver::CFIE,billiard::Bi,k::T;use_combined::Bool=false) where {Bi<:AbsBilliard,T<:Real}
+    pts=evaluate_points(solver,billiard,k)
+    N=length(pts.xy)
+    Rmat=zeros(T,N,N)
+    kress_R_fft!(Rmat) # or kress_R_sum!(Rmat) for small N
+    A=M(pts,k,Rmat;use_combined=use_combined)
+    mu=svdvals(A)
+    return mu[end]
 end
 
 ####################
