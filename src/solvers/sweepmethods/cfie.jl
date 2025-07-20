01976b051484408b848c87800772b15f3dc1ad91
MathiasTensor <tensor1995@gmail.com>
2025-07-20 13:57:28 +0200

    Update cfie.jl

--- src/solvers/sweepmethods/cfie.jl
+++ src/solvers/sweepmethods/cfie.jl
@@ -424,10 +424,10 @@ function solve_INFO(solver::Union{CFIE_polar_nocorners,CFIE_polar_corner_correct
     svd_time=t4-t3
     total=build_R+build_A+svd_time
     println("────────── SOLVE_INFO SUMMARY ──────────")
-    println(" R-matrix build:  %6.2f %%",100*build_R/total)
-    println(" A-matrix build:  %6.2f %%",100*build_A/total)
-    println(" SVD:             %6.2f %%",100*svd_time/total)
-    println(" (total = %.3f s)",total)
+    println(" R-matrix build: ",100*build_R/total," %")
+    println(" A-matrix build: ",100*build_A/total," %")
+    println(" SVD: ",100*svd_time/total," %")
+    println(" (total: ",total," s")
     println("─────────────────────────────────────────")
     return s[end]
 end
