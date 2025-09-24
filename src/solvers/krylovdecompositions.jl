fc9ce355df7da566c2256ad09c7016584ca57726
MathiasTensor <tensor1995@gmail.com>
2025-09-24 18:17:11 +0200

    Update krylovdecompositions.jl

--- src/solvers/krylovdecompositions.jl
+++ src/solvers/krylovdecompositions.jl
@@ -1,3 +1,6 @@
+using KrylovKit
+using LinearMaps
+using LinearAlgebra
 
 function solve_krylov(solver::BoundaryIntegralMethod,basis::Ba,pts::BoundaryPointsBIM,k;kernel_fun::Union{Symbol,Function}=:default,multithreaded::Bool=true) where {Ba<:AbstractHankelBasis}
     A=construct_matrices(solver,basis,pts,k;kernel_fun=kernel_fun,multithreaded=multithreaded)
