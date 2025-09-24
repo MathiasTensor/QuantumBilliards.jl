9cae550cf90bb2793e47588177cc7c3ff008d88d
MathiasTensor <tensor1995@gmail.com>
2025-09-24 17:32:10 +0200

    No need for extra module

--- src/QuantumBilliards.jl
+++ src/QuantumBilliards.jl
@@ -96,7 +96,7 @@ include("solvers/sweepmethods/boundaryintegralmethod.jl")
 include("solvers/sweepmethods/expanded_boundary_integral_method.jl")
 include("solvers/sweepmethods/sweepmethods.jl")
 include("solvers/sweepmethods/cfie.jl")
-include("../ext/KrylovExternal.jl")
+include("solvers/KrylovExternal.jl")
 export ScalingMethodA, ScalingMethodB 
 export DecompositionMethod, ParticularSolutionsMethod
 export BoundaryPointsSM, BoundaryPointsDM
--- src/solvers/KrylovExternal.jl
+++ src/solvers/KrylovExternal.jl
@@ -1,11 +1,6 @@
-module KrylovExternal
-using QuantumBilliards
-using KrylovKit,LinearMaps,LinearAlgebra
 
 function _solve_krylov(solver::BoundaryIntegralMethod,basis::Ba,pts::BoundaryPointsBIM,k;kernel_fun::Union{Symbol,Function}=:default,multithreaded::Bool=true) where {Ba<:AbstractHankelBasis}
     A=construct_matrices(solver,basis,pts,k;kernel_fun=kernel_fun,multithreaded=multithreaded)
     vals,_,_,_=svdsolve(A,1,:SR)
     return vals[1]
 end
-
-end # module
