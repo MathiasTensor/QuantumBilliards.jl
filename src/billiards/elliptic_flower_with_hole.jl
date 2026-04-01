c7adf246e15284e32671cf6b957ea17789f345af
MathiasTensor <tensor1995@gmail.com>
2026-04-01 19:03:44 +0200

    Update elliptic_flower_with_hole.jl

--- src/billiards/elliptic_flower_with_hole.jl
+++ src/billiards/elliptic_flower_with_hole.jl
@@ -23,7 +23,7 @@ function elliptic_arc(A::SVector{2,T},B::SVector{2,T},b::T;origin=zero(A),rot_an
     mp_minus=C - b*v
     s = norm(mp_plus) > norm(mp_minus) ? one(T) : -one(T)
     r_func=t->begin
-        t=1-t
+        #t=1-t
         θ=T(pi)*(one(T)-t) 
         C + a*cos(θ)*u + s*b*sin(θ)*v
     end
