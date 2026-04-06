56012af1b6dca37e1ab2ec0f76c35137b117ca9f
MathiasTensor <tensor1995@gmail.com>
2026-04-06 18:13:01 +0200

    Update realplanewaves.jl

--- src/basis/planewaves/realplanewaves.jl
+++ src/basis/planewaves/realplanewaves.jl
@@ -46,7 +46,6 @@ function parity_pattern(symmetries)
             parity_x=[sym.parity[1]]
             parity_y=[sym.parity[2]]
             xy_reflected=true
-            break  # Once XYReflection is applied, we must exit as there is nothing more to be done
         end
     end
     if xy_reflected
