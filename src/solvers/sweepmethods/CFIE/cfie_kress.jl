6276c3c9deb98ed3459ca4510c1582ed60a857ab
MathiasTensor <tensor1995@gmail.com>
2026-04-08 11:45:17 +0200

    same - sign ossue with curvature

--- src/solvers/sweepmethods/CFIE/cfie_kress.jl
+++ src/solvers/sweepmethods/CFIE/cfie_kress.jl
@@ -252,7 +252,7 @@ function construct_matrices!(solver::CFIE_kress,A::Matrix{Complex{T}},pts::Vecto
             si=Ga.speed[i]
             κi=Ga.kappa[i]
             wi=pa.ws[i]
-            dval= -Complex{T}(wi*κi,zero(T))
+            dval= -Complex{T}(wi*κi,zero(T)) # +/- horrible diabmiguity here due to how we construct the final matrix. Took forever to debug this
             m1=αM1*si
             m2=((Complex{T}(0,one(T)/2)-euler_over_pi)-inv_two_pi*log((k^2/4)*si^2))*si
             sval=Complex{T}(Rmat[gi,gi]*m1,zero(T))+wi*m2
