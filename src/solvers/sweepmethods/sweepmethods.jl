137393009ab782f16d170f6fb0e2a585e49d41e4
MathiasTensor <tensor1995@gmail.com>
2026-04-14 15:22:08 +0200

    Update sweepmethods.jl

--- src/solvers/sweepmethods/sweepmethods.jl
+++ src/solvers/sweepmethods/sweepmethods.jl
@@ -87,7 +87,7 @@ end
 end
 
 """
-        k_sweep(solver::SweepSolver,basis::AbsBasis,billiard::AbsBilliard,ks;multithreaded_matrices::Bool=true,multithreaded_ks::Bool=false,use_krylov::Bool=true,tol=1e-10)
+        k_sweep(solver::SweepSolver,basis::AbsBasis,billiard::AbsBilliard,ks;multithreaded_matrices::Bool=true,use_krylov::Bool=true,tol=1e-10)
 
 High-level API for performing a sweep over wavenumbers `ks` to compute tensions. The function dispatches to the appropriate internal `_k_sweep` method based on the type of `solver`. It supports multithreading for both matrix construction and wavenumber sweeps, and allows for the use of Krylov solvers where applicable.
 
@@ -97,19 +97,18 @@ High-level API for performing a sweep over wavenumbers `ks` to compute tensions.
 - `billiard::AbsBilliard`: The billiard configuration.
 - `ks::AbstractVector{T}`: Vector of wavenumbers to sweep over.
 - `multithreaded_matrices::Bool=true`: Whether to use multithreading for matrix construction.
-- `multithreaded_ks::Bool=false`: Whether to use multithreading for the wavenumber sweep.
 - `use_krylov::Bool=true`: Whether to use Krylov solvers where applicable.
 - `tol::Real=1e-10`: Tolerance for convergence in appropriate solvers (`ParticularSolutionsMethod`).
 - `which::Symbol=:svd`: Whether to compute the determinant (`:det`) or the smallest singular value (`:svd`) during refinement. Also there is option :det_argmin which can be used for finding minima.
 """
-function k_sweep(solver,basis::AbsBasis,billiard::AbsBilliard,ks;multithreaded_matrices::Bool=true,multithreaded_ks::Bool=false,use_krylov::Bool=true,tol=1e-10,which::Symbol=:det_argmin)
-    solve_first,solve_one=_k_sweep_prepare(solver,basis,billiard,ks;multithreaded_matrices=multithreaded_matrices,multithreaded_ks=multithreaded_ks,use_krylov=use_krylov,tol=tol,which=which)
+function k_sweep(solver,basis::AbsBasis,billiard::AbsBilliard,ks;multithreaded_matrices::Bool=true,use_krylov::Bool=true,tol=1e-10,which::Symbol=:det_argmin)
+    solve_first,solve_one=_k_sweep_prepare(solver,basis,billiard,ks;multithreaded_matrices=multithreaded_matrices,use_krylov=use_krylov,tol=tol,which=which)
     res=_k_sweep_result_container(ks;which=which)
     println("$(nameof(typeof(solver))) sweep...")
     p=Progress(length(ks),1)
     res[end]=solve_first(ks[end])
     next!(p)
-    @use_threads multithreading=multithreaded_ks for i in eachindex(ks)[1:end-1]
+    for i in eachindex(ks)[1:end-1]
         is_calculating=true
         while is_calculating
             try
