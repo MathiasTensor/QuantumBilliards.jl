ec50e53474b4d1b70f1d3be8fb1a60f1d4839c06
MathiasTensor <tensor1995@gmail.com>
2026-06-09 15:43:27 +0200

    Update dlp_hyperbolic_gl_quad.jl

--- src/hyperbolic/dlp_hyperbolic_gl_quad.jl
+++ src/hyperbolic/dlp_hyperbolic_gl_quad.jl
@@ -845,44 +845,44 @@ function construct_adjoint_dlp_hyp_log_product_matrix!(Ddag::AbstractMatrix{Comp
     fill!(Ddag,zero(Complex{T}))
     @use_threads multithreading=(multithreaded&&N>=32) for j in 1:N
         dsj=ds[j]
-        p_j=pdata.panel_id[j]
-        jl=pdata.local_id[j]
-        for i in 1:N
+        @inbounds for i in 1:N
             dsi=ds[i]
+            # Reconstruct exact source entry D[j,i], then weight-adjoint it.
             if i==j
-                Ddag[i,i]=dsi*hyp_L2_diag_GL(pts.kappa[i],G.dnlogλ[i])
+                Dji=pts.ds[i]*hyp_L2_diag_GL(pts.kappa[j],G.dnlogλ[j])
             else
-                p_i=pdata.panel_id[i]
-                il=pdata.local_id[i]
-                speed_half_i=G.speed_half[i]
+                p_row=pdata.panel_id[j]
+                p_col=pdata.panel_id[i]
+                row_local=pdata.local_id[j]
+                col_local=pdata.local_id[i]
+                speed_half_col=G.speed_half[i]
                 d=G.d[j,i]
                 dn=G.dn[j,i]
-                Dji=zero(Complex{T})
-                if p_i==p_j
-                    l1=2*hyp_L1(ptab,Float64(d),dn)*speed_half_i
-                    full=hyp_raw_dlp(qtab,Float64(d),dn)*speed_half_i
-                    l2=full-l1*log(abs(ξ[jl]-ξ[il]))
-                    Dji=Λ[jl,il]*l1+ω[il]*l2
+                if p_row==p_col
+                    l1=2*hyp_L1(ptab,Float64(d),dn)*speed_half_col
+                    full=hyp_raw_dlp(qtab,Float64(d),dn)*speed_half_col
+                    l2=full-l1*log(abs(ξ[row_local]-ξ[col_local]))
+                    Dji=Λ[row_local,col_local]*l1+ω[col_local]*l2
                 else
-                    r=cyclic_panel_offset(p_j,p_i,npan)
-                    if abs(r)<=near_panels&&haskey(nearΛ,r)
-                        x0=ξ[jl]+T(2*r)
-                        l1=2*hyp_L1(ptab,Float64(d),dn)*speed_half_i
-                        full=hyp_raw_dlp(qtab,Float64(d),dn)*speed_half_i
-                        l2=full-l1*log(abs(ξ[il]-x0))
-                        Dji=nearΛ[r][jl,il]*l1+ω[il]*l2
+                    r=cyclic_panel_offset(p_row,p_col,npan)
+                    if abs(r)<=near_panels && haskey(nearΛ,r)
+                        x0=ξ[row_local]+T(2*r)
+                        l1=2*hyp_L1(ptab,Float64(d),dn)*speed_half_col
+                        full=hyp_raw_dlp(qtab,Float64(d),dn)*speed_half_col
+                        l2=full-l1*log(abs(ξ[col_local]-x0))
+                        Dji=nearΛ[r][row_local,col_local]*l1+ω[col_local]*l2
                     else
-                        Dji=hyp_raw_dlp(qtab,Float64(d),dn)*dsi
+                        Dji=hyp_raw_dlp(qtab,Float64(d),dn)*pts.ds[i]
                     end
                 end
-                @inbounds for rimg in eachindex(G.imgscale)
-                    dI=G.dimg[rimg][j,i]
-                    dI<=eps(T)&&continue
-                    dnI=G.dnimg[rimg][j,i]
-                    Dji+=G.imgscale[rimg]*hyp_raw_dlp(qtab,Float64(dI),dnI)*dsi
-                end
-                Ddag[i,j]=Dji*(dsj/dsi)
             end
+            @inbounds for rimg in eachindex(G.imgscale)
+                dI=G.dimg[rimg][j,i]
+                dI<=eps(T)&&continue
+                dnI=G.dnimg[rimg][j,i]
+                Dji+=G.imgscale[rimg]*hyp_raw_dlp(qtab,Float64(dI),dnI)*pts.ds[i]
+            end
+            Ddag[i,j]=Dji*(dsj/dsi)
         end
     end
     return Ddag
