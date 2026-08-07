61388e70ecc4e7b8201507ad6730e55ecd70cea5
MathiasTensor <tensor1995@gmail.com>
2026-08-07 11:28:25 +0200

    Update special_functions_calls.jl

--- src/utils/special_functions_calls.jl
+++ src/utils/special_functions_calls.jl
@@ -5,5 +5,7 @@
 @inline J(n::Int,x::T) where {T<:Real}=Bessels.besselj(n,x)
 @inline J(n::Int,x::Complex{T}) where {T<:Real}=SpecialFunctions.besselj(n,x)
 @inline function hankel_pair01(x);h0=H(0,x);h1=H(1,x);return h0,h1;end
-@inline Φ_helmholtz(k::T,r::T) where {T<:Real}=(im/4)*Bessels.hankelh1(0,k*r) # used for CFIE solvers to get the normal derivative of the wavefunctions
+@inline function bessel_pair01(x);j0=J(0,x);j1=J(1,x);return j0,j1;end
+@inline Φ_helmholtz(k::T,r::T) where {T<:Real}=(im/4)*H(0,k*r) # used for CFIE solvers to get the normal derivative of the wavefunctions
 # this is the slp kernel used in the hypersingular maue kress formula for the normal derivative of the DLP kernel.
+@inline Φ_helmholtz(k::Complex{T},r::T) where {T<:Real}=(im/4)*H(0,k*r)
