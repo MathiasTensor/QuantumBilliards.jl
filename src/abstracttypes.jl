daa3cdabaae886b61528f1fda59773a7a26b1e05
Crt Lozej <clozej@gmail.com>
2026-08-15 17:31:15 +0200

    Cleanup residual types

--- src/abstracttypes.jl
+++ src/abstracttypes.jl
@@ -2,13 +2,8 @@
 abstract type CoordinateSystem end
 abstract type AbsBasis end
 abstract type AbsSolver end
-abstract type AbsPoints end
 abstract type SweepSolver <: AbsSolver end
 abstract type AcceleratedSolver <: AbsSolver end
 
 abstract type AbsState end
 abstract type StationaryState <: AbsState end
-
-#grid iterators
-abstract type AbsGrid end
- 
