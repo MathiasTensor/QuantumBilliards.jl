75404114d33bcb06dca58141a8d2492329fede80
MathiasTensor <tensor1995@gmail.com>
2026-08-27 17:18:27 +0200

    Update sweepmethods.jl

--- src/solvers/sweepmethods/sweepmethods.jl
+++ src/solvers/sweepmethods/sweepmethods.jl
@@ -1,4 +1,4 @@
-function solve_wavenumber(solver::Union{BoundaryIntegralMethod,DLP_kress,DLP_kress_global_corners,CFIE_kress,CFIE_kress_corners,CFIE_kress_global_corners},basis::AbstractHankelBasis(),billiard::AbsBilliard,k,dk;multithreaded::Bool=true,use_krylov::Bool=false,which::Symbol=:det_argmin)
+function solve_wavenumber(solver::Union{BoundaryIntegralMethod,DLP_kress,DLP_kress_global_corners,CFIE_kress,CFIE_kress_corners,CFIE_kress_global_corners},basis::Ba,billiard::Bi,k,dk;multithreaded::Bool=true,use_krylov::Bool=false,which::Symbol=:det_argmin) where {Ba<:AbsBasis,Bi<:BilliardGeometry.AbsBilliard}
     pts=evaluate_points(solver,billiard,k)
     f(kk)=solve(solver,basis,pts,kk;multithreaded=multithreaded,use_krylov=use_krylov,which=which)
     res=Optim.optimize(f,k-dk/2,k+dk/2)
