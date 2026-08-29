e017b7e879eda91713afc6bbefb20df1e4161ddc
MathiasTensor <tensor1995@gmail.com>
2026-08-29 23:28:07 +0200

    Update symmetry.jl

--- src/states/symmetry/symmetry.jl
+++ src/states/symmetry/symmetry.jl
@@ -11,8 +11,8 @@ end
 DiagonalReflection()=DiagonalReflection(-1)
 AntiDiagonalReflection()=AntiDiagonalReflection(-1)
 
-const reflect_diag=LinearMap(SMatrix{2,2}([0.0 1.0;1.0 0.0]))
-const reflect_antidiag=LinearMap(SMatrix{2,2}([0.0 -1.0;-1.0 0.0]))
+const reflect_diag=CoordinateTransformations.LinearMap(SMatrix{2,2}([0.0 1.0;1.0 0.0]))
+const reflect_antidiag=CoordinateTransformations.LinearMap(SMatrix{2,2}([0.0 -1.0;-1.0 0.0]))
 
 @inline symmetry_irrep_character(::Type{T},sym::DiagonalReflection) where {T<:Real}=Complex{T}(sym.parity)
 @inline symmetry_irrep_character(::Type{T},sym::AntiDiagonalReflection) where {T<:Real}=Complex{T}(sym.parity)
