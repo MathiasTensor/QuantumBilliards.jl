9387af29bc89d840d4fd41505466269cd0ecd5a7
MathiasTensor <tensor1995@gmail.com>
2026-09-01 09:44:40 +0200

    Removed stuff that was moved to BilliardGeometry.jl

--- src/QuantumBilliards.jl
+++ src/QuantumBilliards.jl
@@ -34,8 +34,6 @@ export try_MKL!
 include("utils/coordinatesystems.jl")
 export CartesianCS,PolarCS,polar_to_cartesian,cartesian_to_polar
 
-include("utils/curve_derivatives.jl")
-export tangent,tangent_2,tangent_vec,normal_vec,curvature
 include("solvers/samplers.jl")
 include("utils/kress_grading_single_corner.jl")
 include("utils/kress_grading_multi_corner.jl")
